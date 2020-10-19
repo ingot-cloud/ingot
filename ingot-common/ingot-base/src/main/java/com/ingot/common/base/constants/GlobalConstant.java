94aaa38e1c41990e6dfbf59b95d7d237afe53bed
wangchao <magician.of.technique@gmail.com>
2020-10-19 14:02:06 +0800

    [*] 常量使用接口

--- ingot-common/ingot-base/src/main/java/com/ingot/common/base/constants/GlobalConstant.java
+++ ingot-common/ingot-base/src/main/java/com/ingot/common/base/constants/GlobalConstant.java
@@ -6,41 +6,41 @@ package com.ingot.common.base.constants;
  * <p>Date         : 2018/5/4.</p>
  * <p>Time         : 上午11:24.</p>
  */
-public class GlobalConstant {
+public interface GlobalConstant {
 
-    public static final String UNKNOWN = "unknown";
+    String UNKNOWN = "unknown";
 
-    public static final String X_FORWARDED_FOR = "X-Forwarded-For";
-    public static final String X_REAL_IP = "X-Real-IP";
-    public static final String PROXY_CLIENT_IP = "Proxy-Client-IP";
-    public static final String WL_PROXY_CLIENT_IP = "WL-Proxy-Client-IP";
-    public static final String HTTP_CLIENT_IP = "HTTP_CLIENT_IP";
-    public static final String HTTP_X_FORWARDED_FOR = "HTTP_X_FORWARDED_FOR";
+    String X_FORWARDED_FOR = "X-Forwarded-For";
+    String X_REAL_IP = "X-Real-IP";
+    String PROXY_CLIENT_IP = "Proxy-Client-IP";
+    String WL_PROXY_CLIENT_IP = "WL-Proxy-Client-IP";
+    String HTTP_CLIENT_IP = "HTTP_CLIENT_IP";
+    String HTTP_X_FORWARDED_FOR = "HTTP_X_FORWARDED_FOR";
 
-    public static final String LOCALHOST_IP = "127.0.0.1";
-    public static final String LOCALHOST_IP_16 = "0:0:0:0:0:0:0:1";
-    public static final int MAX_IP_LENGTH = 15;
+    String LOCALHOST_IP = "127.0.0.1";
+    String LOCALHOST_IP_16 = "0:0:0:0:0:0:0:1";
+    int MAX_IP_LENGTH = 15;
 
-    public static final String COMMA = ",";
-    public static final String SPOT = ".";
+    String COMMA = ",";
+    String SPOT = ".";
     /**
      * The constant UNDER_LINE.
      */
-    public final static String UNDER_LINE = "_";
+    String UNDER_LINE = "_";
     /**
      * The constant PER_CENT.
      */
-    public final static String PER_CENT = "%";
+    String PER_CENT = "%";
     /**
      * The constant AT.
      */
-    public final static String AT = "@";
+    String AT = "@";
     /**
      * The constant PIPE.
      */
-    public final static String PIPE = "||";
-    public final static String SHORT_LINE = "-";
-    public final static String SPACE = " ";
-    public static final String SLASH = "/";
-    public static final String MH = ":";
+    String PIPE = "||";
+    String SHORT_LINE = "-";
+    String SPACE = " ";
+    String SLASH = "/";
+    String MH = ":";
 }
