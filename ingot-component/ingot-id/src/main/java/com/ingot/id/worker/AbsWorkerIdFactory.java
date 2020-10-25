ef9fc332640034b78feede869786711028f77ec0
wangchao <magician.of.technique@gmail.com>
2020-10-25 15:23:18 +0800

    [*] AbsWorkerIdFactory

--- ingot-component/ingot-id/src/main/java/com/ingot/id/worker/AbsWorkerIdFactory.java
+++ ingot-component/ingot-id/src/main/java/com/ingot/id/worker/AbsWorkerIdFactory.java
@@ -1,7 +1,77 @@
-package com.ingot.id.worker;/**
+package com.ingot.id.worker;
+
+import cn.hutool.core.io.FileUtil;
+import cn.hutool.core.io.IORuntimeException;
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+/**
  * <p>Description  : AbsWorkerIdFactory.</p>
  * <p>Author       : wangchao.</p>
  * <p>Date         : 2020/10/25.</p>
  * <p>Time         : 3:07 下午.</p>
- */public class AbsWorkerIdFactory {
+ */
+@Slf4j
+public abstract class AbsWorkerIdFactory implements WorkerIdFactory {
+    private final String LOCAL_PROP_PATH;
+
+    protected final String ip;
+    protected final String port;
+
+    public AbsWorkerIdFactory(String ip, String port, String serviceName) {
+        this.ip = ip;
+        this.port = port;
+        this.LOCAL_PROP_PATH = "/data/ingot/id/" + serviceName + "/{port}/workerID.properties";
+    }
+
+    /**
+     * 获取缓存的 work id
+     *
+     * @return 本地文件存储的 work id
+     * @throws Exception 获取失败
+     */
+    protected int getCacheWorkId() throws Exception {
+        Properties properties = new Properties();
+        properties.load(new FileInputStream(new File(LOCAL_PROP_PATH.replace("{port}", port + ""))));
+        return Integer.parseInt(properties.getProperty("workerID"));
+    }
+
+    /**
+     * 在节点文件系统上缓存一个 work id 值
+     *
+     * @param workerID worker id
+     */
+    protected void updateLocalWorkerID(int workerID) {
+        File confFile = new File(LOCAL_PROP_PATH.replace("{port}", port));
+        boolean exists = confFile.exists();
+        log.info(">>> AbsWorkerIdFactory - file exists status is {}", exists);
+        if (exists) {
+            try {
+                FileUtil.writeUtf8String("workerID=" + workerID, confFile);
+                log.info(">>> AbsWorkerIdFactory - update file cache workerID is {}", workerID);
+            } catch (IORuntimeException e) {
+                log.error(">>> AbsWorkerIdFactory - update file cache error ", e);
+            }
+        } else {
+            //不存在文件,父目录页肯定不存在
+            try {
+                boolean mkdirs = confFile.getParentFile().mkdirs();
+                log.info(">>> AbsWorkerIdFactory - init local file cache create parent dis status is {}, worker id is {}", mkdirs, workerID);
+                if (mkdirs) {
+                    if (confFile.createNewFile()) {
+                        FileUtil.writeUtf8String("workerID=" + workerID, confFile);
+                        log.info(">>> AbsWorkerIdFactory - local file cache workerID is {}", workerID);
+                    }
+                } else {
+                    log.warn(">>> AbsWorkerIdFactory - create parent dir error");
+                }
+            } catch (IOException e) {
+                log.warn(">>> AbsWorkerIdFactory - create workerID conf file error", e);
+            }
+        }
+    }
 }
