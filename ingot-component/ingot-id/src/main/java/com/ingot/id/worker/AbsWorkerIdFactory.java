a5db0fa74e7835e735c0a82de3bd6779b1062ed8
wangchao <magician.of.technique@gmail.com>
2020-10-25 15:19:46 +0800

    [*] WorkerIdFactory 抽象类

---
+++ ingot-component/ingot-id/src/main/java/com/ingot/id/worker/AbsWorkerIdFactory.java
@@ -0,0 +1,7 @@
+package com.ingot.id.worker;/**
+ * <p>Description  : AbsWorkerIdFactory.</p>
+ * <p>Author       : wangchao.</p>
+ * <p>Date         : 2020/10/25.</p>
+ * <p>Time         : 3:07 下午.</p>
+ */public class AbsWorkerIdFactory {
+}
