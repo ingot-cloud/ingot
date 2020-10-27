0146bae36fb93abea03f38299c3f45e8ef5405ca
wangchao <magician.of.technique@gmail.com>
2020-10-27 13:51:26 +0800

    [+] ingot-acs

---
+++ ingot-service/ingot-acs/src/main/java/com/ingot/cloud/acs/IngotAcsApplication.java
@@ -0,0 +1,13 @@
+package com.ingot.cloud.acs;
+
+import org.springframework.cloud.client.SpringCloudApplication;
+
+/**
+ * <p>Description  : IngotAcsApplication.</p>
+ * <p>Author       : wangchao.</p>
+ * <p>Date         : 2020/10/27.</p>
+ * <p>Time         : 11:37 上午.</p>
+ */
+@SpringCloudApplication
+public class IngotAcsApplication {
+}
