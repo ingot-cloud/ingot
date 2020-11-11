b59414fa24f5c918c93d9a5ab6fc7d3cdb998417
wangchao <magician.of.technique@gmail.com>
2020-11-11 18:49:27 +0800

    [*] 优化获取 UserDetailApi

---
+++ ingot-service/ingot-pms/ingot-pms-provider/src/main/java/com/ingot/cloud/pms/rest/UserApi.java
@@ -0,0 +1,16 @@
+package com.ingot.cloud.pms.rest;
+
+import com.ingot.framework.core.wrapper.BaseController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * <p>Description  : UserApi.</p>
+ * <p>Author       : wangchao.</p>
+ * <p>Date         : 2020/11/11.</p>
+ * <p>Time         : 6:48 下午.</p>
+ */
+@RestController
+@RequestMapping(value = "/user")
+public class UserApi extends BaseController {
+}
--- ingot-service/ingot-pms/ingot-pms-provider/src/main/java/com/ingot/cloud/pms/rest/UserDetailApi.java
+++ ingot-service/ingot-pms/ingot-pms-provider/src/main/java/com/ingot/cloud/pms/rest/UserDetailApi.java
@@ -5,15 +5,14 @@ import com.ingot.framework.core.model.dto.user.UserDetailsDto;
 import com.ingot.framework.core.wrapper.BaseController;
 import com.ingot.framework.core.wrapper.IngotResponse;
 import com.ingot.framework.security.annotation.Permit;
+import com.ingot.framework.security.model.enums.PermitModel;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import static com.ingot.framework.security.model.enums.PermitModel.INNER;
-
 /**
- * <p>Description  : UserApi.</p>
+ * <p>Description  : UserDetailApi.</p>
  * <p>Author       : wangchao.</p>
  * <p>Date         : 2020/11/4.</p>
  * <p>Time         : 4:41 下午.</p>
@@ -22,7 +21,7 @@ import static com.ingot.framework.security.model.enums.PermitModel.INNER;
 @RequestMapping(value = "/user")
 public class UserDetailApi extends BaseController {
 
-    @Permit(model = INNER)
+    @Permit(model = PermitModel.INNER)
     @PostMapping(value = "/detail")
     IngotResponse<UserAuthDetails> getUserAuthDetail(@RequestBody UserDetailsDto params){
         // todo 请求参数封装
