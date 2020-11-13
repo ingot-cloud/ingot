1dcbc1c7cf4cad261d2cb3ff858972a575a9c6af
wangchao <magician.of.technique@gmail.com>
2020-11-13 17:47:11 +0800

    [*] 修改后数据库名称

---
+++ ingot-service/ingot-pms/ingot-pms-provider/src/main/java/com/ingot/cloud/pms/service/UserService.java
@@ -0,0 +1,10 @@
+package com.ingot.cloud.pms.service;
+
+/**
+ * <p>Description  : UserService.</p>
+ * <p>Author       : wangchao.</p>
+ * <p>Date         : 2020/11/12.</p>
+ * <p>Time         : 8:04 下午.</p>
+ */
+public interface UserService {
+}
