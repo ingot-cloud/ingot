fab3d3ebb943f0312f0519dfed7177c42c874607
wangchao <magician.of.technique@gmail.com>
2020-11-13 19:05:48 +0800

    [*] SysUser 增加注解

--- ingot-service/ingot-pms/ingot-pms-provider/src/main/java/com/ingot/cloud/pms/model/domain/SysUser.java
+++ ingot-service/ingot-pms/ingot-pms-provider/src/main/java/com/ingot/cloud/pms/model/domain/SysUser.java
@@ -1,11 +1,16 @@
 package com.ingot.cloud.pms.model.domain;
 
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableLogic;
 import com.baomidou.mybatisplus.annotation.TableName;
+import com.baomidou.mybatisplus.annotation.Version;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.ingot.framework.store.mybatis.model.BaseModel;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import org.springframework.format.annotation.DateTimeFormat;
 
-import java.time.LocalDateTime;
+import java.util.Date;
 
 /**
  * <p>
@@ -25,11 +30,13 @@ public class SysUser extends BaseModel<SysUser> {
     /**
      * ID
      */
+    @TableId
     private Long id;
 
     /**
      * 版本号
      */
+    @Version
     private Long version;
 
     /**
@@ -70,17 +77,24 @@ public class SysUser extends BaseModel<SysUser> {
     /**
      * 创建日期
      */
-    private LocalDateTime createdAt;
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
+    private Date createdAt;
 
     /**
      * 更新日期
      */
-    private LocalDateTime updatedAt;
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
+    private Date updatedAt;
 
     /**
      * 删除日期
      */
-    private LocalDateTime deletedAt;
+    @TableLogic
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
+    private Date deletedAt;
 
 
 }
