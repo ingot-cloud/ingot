34c0e55154fb18cfb719c35890e9edf60e892d8e
wangchao <magician.of.technique@gmail.com>
2020-11-20 16:37:17 +0800

    [*] 修改服务名称

--- ingot-framework/ingot-core/src/main/java/com/ingot/framework/core/constants/ServiceNameConstants.java
+++ ingot-framework/ingot-core/src/main/java/com/ingot/framework/core/constants/ServiceNameConstants.java
@@ -11,40 +11,40 @@ public interface ServiceNameConstants {
     /**
      * 鉴权中心
      */
-    String AC_SERVICE = "ingot-client-ac";
+    String AC_SERVICE = "ingot-ac";
 
     /**
      * 权限管理系统
      */
-    String PMS_SERVICE = "ingot-client-pms";
+    String PMS_SERVICE = "ingot-pms";
 
     /**
      * 对接中心
      */
-    String DMC_SERVICE = "ingot-client-dmc";
+    String DMC_SERVICE = "ingot-dmc";
 
     /**
      * 可靠消息中心
      */
-    String MMC_SERVICE = "ingot-client-mmc";
+    String MMC_SERVICE = "ingot-mmc";
 
     /**
      * 任务调度中心
      */
-    String TMC_SERVICE = "ingot-client-tmc";
+    String TMC_SERVICE = "ingot-tmc";
 
     /**
      * 数据中心
      */
-    String DC_SERVICE = "ingot-client-dc";
+    String DC_SERVICE = "ingot-dc";
 
     /**
      * Srs 服务
      */
-    String SRS_SERVICE = "ingot-client-srs";
+    String SRS_SERVICE = "ingot-srs";
 
     /**
      * Srs lite 服务
      */
-    String SRS_LITE_SERVICE = "ingot-client-srslite";
+    String SRS_LITE_SERVICE = "ingot-srslite";
 }
