756209ae9b40c7b734dcf9390a38eeeb21ff5447
wangchao <magician.of.technique@gmail.com>
2021-01-01 15:27:31 +0800

    [*] 优化代码

--- ingot-service/ingot-pms/ingot-pms-provider/src/main/java/com/ingot/cloud/pms/service/SysRoleService.java
+++ ingot-service/ingot-pms/ingot-pms-provider/src/main/java/com/ingot/cloud/pms/service/SysRoleService.java
@@ -13,4 +13,11 @@ import com.ingot.framework.store.mybatis.service.BaseService;
  */
 public interface SysRoleService extends BaseService<SysRole> {
 
+//    /**
+//     * 获取用户所有角色，包括
+//     * @param tenantId 租户ID
+//     * @param userId 用户ID
+//     * @return
+//     */
+//    List<SysRole> getAllRolesOfUser(long tenantId, long userId);
 }
