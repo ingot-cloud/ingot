baf3d51c7a1e36b49695cf7d82793949901891bb
wangchao <magician.of.technique@gmail.com>
2021-01-04 20:14:45 +0800

    [+] gateway

---
+++ ingot-service/ingot-gateway/src/main/java/com/ingot/cloud/gateway/IngotGatewayApplication.java
@@ -0,0 +1,10 @@
+package com.ingot.cloud.gateway;
+
+/**
+ * <p>Description  : IngotGatewayApplication.</p>
+ * <p>Author       : wangchao.</p>
+ * <p>Date         : 2021/1/4.</p>
+ * <p>Time         : 8:01 下午.</p>
+ */
+public class IngotGatewayApplication {
+}
