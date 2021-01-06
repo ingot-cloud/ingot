2d83a45c649203c8278444b5f02b31d4ac175011
wangchao <magician.of.technique@gmail.com>
2021-01-06 10:34:40 +0800

    [*] SysUser json 序列化忽略 version 和 password 字段

--- ingot-service/ingot-pms/ingot-pms-provider/src/main/java/com/ingot/cloud/pms/model/domain/SysUser.java
+++ ingot-service/ingot-pms/ingot-pms-provider/src/main/java/com/ingot/cloud/pms/model/domain/SysUser.java
@@ -4,6 +4,7 @@ import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableLogic;
 import com.baomidou.mybatisplus.annotation.TableName;
 import com.baomidou.mybatisplus.annotation.Version;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.ingot.framework.core.model.enums.UserStatusEnum;
 import com.ingot.framework.store.mybatis.model.BaseModel;
 import lombok.Data;
@@ -35,6 +36,7 @@ public class SysUser extends BaseModel<SysUser> {
     /**
      * 版本号
      */
+    @JsonIgnore
     @Version
     private Long version;
 
@@ -56,6 +58,7 @@ public class SysUser extends BaseModel<SysUser> {
     /**
      * 密码
      */
+    @JsonIgnore
     private String password;
 
     /**
