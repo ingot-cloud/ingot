3a93c2b8ec91dd179d113933fe2202fbfc2efe7f
wangchao <magician.of.technique@gmail.com>
2021-01-13 20:45:32 +0800

    [*] 修改 IngotOAuth2AccessToken 序列化字段

--- ingot-framework/ingot-security/src/main/java/com/ingot/framework/security/provider/IngotOAuth2AccessTokenSerializer.java
+++ ingot-framework/ingot-security/src/main/java/com/ingot/framework/security/provider/IngotOAuth2AccessTokenSerializer.java
@@ -5,7 +5,6 @@ import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.databind.SerializerProvider;
 import com.fasterxml.jackson.databind.ser.std.StdSerializer;
 import com.ingot.framework.security.provider.token.IngotOAuth2AccessToken;
-import org.springframework.security.oauth2.common.OAuth2AccessToken;
 import org.springframework.security.oauth2.common.OAuth2RefreshToken;
 import org.springframework.util.Assert;
 
@@ -29,16 +28,16 @@ public class IngotOAuth2AccessTokenSerializer extends StdSerializer<IngotOAuth2A
     public void serialize(IngotOAuth2AccessToken token, JsonGenerator jgen, SerializerProvider provider) throws IOException,
             JsonGenerationException {
         jgen.writeStartObject();
-        jgen.writeStringField(OAuth2AccessToken.ACCESS_TOKEN, token.getValue());
-        jgen.writeStringField(OAuth2AccessToken.TOKEN_TYPE, token.getTokenType());
+        jgen.writeStringField(IngotOAuth2AccessToken.ACCESS_TOKEN, token.getValue());
+        jgen.writeStringField(IngotOAuth2AccessToken.TOKEN_TYPE, token.getTokenType());
         OAuth2RefreshToken refreshToken = token.getRefreshToken();
         if (refreshToken != null) {
-            jgen.writeStringField(OAuth2AccessToken.REFRESH_TOKEN, refreshToken.getValue());
+            jgen.writeStringField(IngotOAuth2AccessToken.REFRESH_TOKEN, refreshToken.getValue());
         }
         Date expiration = token.getExpiration();
         if (expiration != null) {
             long now = System.currentTimeMillis();
-            jgen.writeNumberField(OAuth2AccessToken.EXPIRES_IN, (expiration.getTime() - now) / 1000);
+            jgen.writeNumberField(IngotOAuth2AccessToken.EXPIRES_IN, (expiration.getTime() - now) / 1000);
         }
         Set<String> scope = token.getScope();
         if (scope != null && !scope.isEmpty()) {
@@ -48,7 +47,7 @@ public class IngotOAuth2AccessTokenSerializer extends StdSerializer<IngotOAuth2A
                 scopes.append(s);
                 scopes.append(" ");
             }
-            jgen.writeStringField(OAuth2AccessToken.SCOPE, scopes.substring(0, scopes.length() - 1));
+            jgen.writeStringField(IngotOAuth2AccessToken.SCOPE, scopes.substring(0, scopes.length() - 1));
         }
         // 不返回 additional information
 //        Map<String, Object> additionalInformation = token.getAdditionalInformation();
--- ingot-framework/ingot-security/src/main/java/com/ingot/framework/security/provider/token/IngotOAuth2AccessToken.java
+++ ingot-framework/ingot-security/src/main/java/com/ingot/framework/security/provider/token/IngotOAuth2AccessToken.java
@@ -18,38 +18,77 @@ import java.util.Set;
  */
 @AllArgsConstructor
 @JsonSerialize(using = IngotOAuth2AccessTokenSerializer.class)
-public class IngotOAuth2AccessToken implements OAuth2AccessToken{
+public class IngotOAuth2AccessToken implements OAuth2AccessToken {
+    /**
+     * The access token issued by the authorization server. This value is REQUIRED.
+     */
+    public static String ACCESS_TOKEN = "accessToken";
+
+    /**
+     * The type of the token issued as described in <a
+     * href="http://tools.ietf.org/html/draft-ietf-oauth-v2-22#section-7.1">Section 7.1</a>. Value is case insensitive.
+     * This value is REQUIRED.
+     */
+    public static String TOKEN_TYPE = "tokenType";
+
+    /**
+     * The lifetime in seconds of the access token. For example, the value "3600" denotes that the access token will
+     * expire in one hour from the time the response was generated. This value is OPTIONAL.
+     */
+    public static String EXPIRES_IN = "expiresIn";
+
+    /**
+     * The refresh token which can be used to obtain new access tokens using the same authorization grant as described
+     * in <a href="http://tools.ietf.org/html/draft-ietf-oauth-v2-22#section-6">Section 6</a>. This value is OPTIONAL.
+     */
+    public static String REFRESH_TOKEN = "refreshToken";
+
+    /**
+     * The scope of the access token as described by <a
+     * href="http://tools.ietf.org/html/draft-ietf-oauth-v2-22#section-3.3">Section 3.3</a>
+     */
+    public static String SCOPE = "scope";
+
+
     private final OAuth2AccessToken accessToken;
 
-    @Override public Map<String, Object> getAdditionalInformation() {
+    @Override
+    public Map<String, Object> getAdditionalInformation() {
         return accessToken.getAdditionalInformation();
     }
 
-    @Override public Set<String> getScope() {
+    @Override
+    public Set<String> getScope() {
         return accessToken.getScope();
     }
 
-    @Override public OAuth2RefreshToken getRefreshToken() {
+    @Override
+    public OAuth2RefreshToken getRefreshToken() {
         return accessToken.getRefreshToken();
     }
 
-    @Override public String getTokenType() {
+    @Override
+    public String getTokenType() {
         return accessToken.getTokenType();
     }
 
-    @Override public boolean isExpired() {
+    @Override
+    public boolean isExpired() {
         return accessToken.isExpired();
     }
 
-    @Override public Date getExpiration() {
+    @Override
+    public Date getExpiration() {
         return accessToken.getExpiration();
     }
 
-    @Override public int getExpiresIn() {
+    @Override
+    public int getExpiresIn() {
         return accessToken.getExpiresIn();
     }
 
-    @Override public String getValue() {
+    @Override
+    public String getValue() {
         return accessToken.getValue();
     }
 }
