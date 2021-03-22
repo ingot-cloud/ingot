1803df1428dac342a34a7504602dcd5e7c3ab53e
wangchao <magician.of.technique@gmail.com>
2021-03-22 18:00:37 +0800

    [*] 创建角色API

--- ingot-service/ingot-pms/ingot-pms-api/src/main/java/com/ingot/cloud/pms/api/model/domain/SysRole.java
+++ ingot-service/ingot-pms/ingot-pms-api/src/main/java/com/ingot/cloud/pms/api/model/domain/SysRole.java
@@ -6,11 +6,15 @@ import com.baomidou.mybatisplus.annotation.TableName;
 import com.baomidou.mybatisplus.annotation.Version;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.ingot.framework.core.validation.Group;
 import com.ingot.framework.core.model.enums.CommonStatusEnum;
+import com.ingot.framework.core.validation.annotation.CommonStatusValidate;
 import com.ingot.framework.store.mybatis.model.BaseModel;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import java.time.LocalDateTime;
 
 /**
@@ -32,6 +36,7 @@ public class SysRole extends BaseModel<SysRole> {
      * ID
      */
     @TableId
+    @NotNull(message = "角色ID不能为空", groups = {Group.Update.class, Group.Delete.class})
     private Long id;
 
     /**
@@ -50,11 +55,13 @@ public class SysRole extends BaseModel<SysRole> {
     /**
      * 角色名称
      */
+    @NotBlank(message = "角色名称不能为空", groups = Group.Create.class)
     private String name;
 
     /**
      * 角色编码
      */
+    @NotBlank(message = "角色编码不能为空", groups = Group.Create.class)
     private String code;
 
     /**
@@ -65,6 +72,7 @@ public class SysRole extends BaseModel<SysRole> {
     /**
      * 状态, 0:正常，9:禁用
      */
+    @CommonStatusValidate
     private CommonStatusEnum status;
 
     /**
--- ingot-service/ingot-pms/ingot-pms-provider/src/main/java/com/ingot/cloud/pms/rest/v1/RoleApi.java
+++ ingot-service/ingot-pms/ingot-pms-provider/src/main/java/com/ingot/cloud/pms/rest/v1/RoleApi.java
@@ -3,11 +3,15 @@ package com.ingot.cloud.pms.rest.v1;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.ingot.cloud.pms.api.model.domain.SysRole;
 import com.ingot.cloud.pms.service.SysRoleService;
+import com.ingot.component.id.IdGenerator;
+import com.ingot.framework.base.exception.IllegalOperationException;
 import com.ingot.framework.core.wrapper.BaseController;
 import com.ingot.framework.core.wrapper.IngotResponse;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,9 +27,20 @@ import org.springframework.web.bind.annotation.RestController;
 @AllArgsConstructor
 public class RoleApi extends BaseController {
     private final SysRoleService sysRoleService;
+    private final IdGenerator idGenerator;
 
     @GetMapping("page")
     public IngotResponse<?> page(Page<SysRole> page, SysRole condition) {
         return ok(sysRoleService.conditionPage(page, condition));
+    }
+
+    @PostMapping
+    public IngotResponse<?> create(@Validated SysRole params) {
+        params.setId(idGenerator.nextId());
+        boolean result = sysRoleService.save(params);
+        if (!result) {
+            throw new IllegalOperationException("角色创建失败");
+        }
+        return ok();
     }
 }
