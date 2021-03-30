d71b971179d272f3f70c0b8862539e9b60c9e047
wangchao <magician.of.technique@gmail.com>
2021-03-30 22:10:00 +0800

    [*] 部门基本api

---
+++ ingot-service/ingot-pms/ingot-pms-provider/src/main/java/com/ingot/cloud/pms/rest/v1/DeptApi.java
@@ -0,0 +1,48 @@
+package com.ingot.cloud.pms.rest.v1;
+
+import com.ingot.cloud.pms.api.model.domain.SysDept;
+import com.ingot.cloud.pms.service.SysDeptService;
+import com.ingot.framework.core.validation.Group;
+import com.ingot.framework.core.wrapper.BaseController;
+import com.ingot.framework.core.wrapper.IngotResponse;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * <p>Description  : DeptApi.</p>
+ * <p>Author       : wangchao.</p>
+ * <p>Date         : 2021/3/30.</p>
+ * <p>Time         : 10:05 下午.</p>
+ */
+@Slf4j
+@RestController
+@RequestMapping(value = "/v1/dept")
+@AllArgsConstructor
+public class DeptApi extends BaseController {
+    private final SysDeptService sysDeptService;
+
+    @GetMapping("/tree")
+    public IngotResponse<?> tree() {
+        return ok(sysDeptService.tree());
+    }
+
+    @PostMapping
+    public IngotResponse<?> create(@Validated(Group.Create.class) @RequestBody SysDept params) {
+        sysDeptService.createDept(params);
+        return ok();
+    }
+
+    @PutMapping
+    public IngotResponse<?> update(@Validated(Group.Update.class) @RequestBody SysDept params) {
+        sysDeptService.updateDept(params);
+        return ok();
+    }
+
+    @DeleteMapping("/{id}")
+    public IngotResponse<?> removeById(@PathVariable Long id) {
+        sysDeptService.removeDeptById(id);
+        return ok();
+    }
+}
