a419a18ba4d10d97f1ac30d661b65984ddd0af46
wangchao <magician.of.technique@gmail.com>
2021-03-31 19:47:39 +0800

    [*] Api

---
+++ ingot-service/ingot-pms/ingot-pms-provider/src/main/java/com/ingot/cloud/pms/rest/v1/MenuApi.java
@@ -0,0 +1,12 @@
+package com.ingot.cloud.pms.rest.v1;
+
+import com.ingot.framework.core.wrapper.BaseController;
+
+/**
+ * <p>Description  : MenuApi.</p>
+ * <p>Author       : wangchao.</p>
+ * <p>Date         : 2021/3/30.</p>
+ * <p>Time         : 10:12 下午.</p>
+ */
+public class MenuApi extends BaseController {
+}
