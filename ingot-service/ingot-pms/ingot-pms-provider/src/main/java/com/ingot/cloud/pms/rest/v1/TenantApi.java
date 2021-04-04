90e55d0676b70bfd09b2239c6a64ca8e747167ff
wangchao <magician.of.technique@gmail.com>
2021-04-04 19:41:13 +0800

    [*] Tenant page

--- ingot-service/ingot-pms/ingot-pms-provider/src/main/java/com/ingot/cloud/pms/rest/v1/TenantApi.java
+++ ingot-service/ingot-pms/ingot-pms-provider/src/main/java/com/ingot/cloud/pms/rest/v1/TenantApi.java
@@ -1,6 +1,13 @@
 package com.ingot.cloud.pms.rest.v1;
 
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.ingot.cloud.pms.api.model.domain.SysTenant;
+import com.ingot.cloud.pms.service.SysTenantService;
 import com.ingot.framework.core.wrapper.BaseController;
+import com.ingot.framework.core.wrapper.IngotResponse;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * <p>Description  : TenantApi.</p>
@@ -8,5 +15,13 @@ import com.ingot.framework.core.wrapper.BaseController;
  * <p>Date         : 2021/3/30.</p>
  * <p>Time         : 10:13 下午.</p>
  */
+@RestController(value = "/v1/tenant")
+@AllArgsConstructor
 public class TenantApi extends BaseController {
+    private final SysTenantService sysTenantService;
+
+    @GetMapping("/page")
+    public IngotResponse<?> page(Page<SysTenant> page, SysTenant params) {
+        return ok(sysTenantService.conditionPage(page, params));
+    }
 }
