67801d6ef502a97b0f2acb1501c38e8fba4c9f42
wangchao <magician.of.technique@gmail.com>
2021-04-15 10:20:25 +0800

    [*] 修改 MybatisPlusConfig

--- ingot-framework/ingot-store/src/main/java/com/ingot/framework/store/mybatis/config/MybatisPlusConfig.java
+++ ingot-framework/ingot-store/src/main/java/com/ingot/framework/store/mybatis/config/MybatisPlusConfig.java
@@ -1,11 +1,10 @@
 package com.ingot.framework.store.mybatis.config;
 
-import com.baomidou.mybatisplus.extension.plugins.inner.OptimisticLockerInnerInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
 import com.ingot.framework.store.mybatis.plugins.IngotOptimisticLockerInterceptor;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -23,17 +22,10 @@ import javax.sql.DataSource;
 public class MybatisPlusConfig {
 
     @Bean
-    public OptimisticLockerInnerInterceptor optimisticLockerInterceptor() {
-        return new IngotOptimisticLockerInterceptor();
-    }
-
-    /**
-     * 分页插件
-     * @return PaginationInterceptor
-     */
-    @Bean
-    @ConditionalOnMissingBean
-    public PaginationInnerInterceptor paginationInterceptor() {
-        return new PaginationInnerInterceptor();
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor mybatisPlusInterceptor = new MybatisPlusInterceptor();
+        mybatisPlusInterceptor.addInnerInterceptor(new IngotOptimisticLockerInterceptor());
+        mybatisPlusInterceptor.addInnerInterceptor(new PaginationInnerInterceptor());
+        return mybatisPlusInterceptor;
     }
 }
