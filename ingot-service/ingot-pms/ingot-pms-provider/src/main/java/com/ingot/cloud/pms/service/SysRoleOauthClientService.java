8a69f13b6e12273e1571a0855ca27cb0bcee0c56
wangchao <magician.of.technique@gmail.com>
2021-05-30 19:35:33 +0800

    [*] 设置绑定服务

--- ingot-service/ingot-pms/ingot-pms-provider/src/main/java/com/ingot/cloud/pms/service/SysRoleOauthClientService.java
+++ ingot-service/ingot-pms/ingot-pms-provider/src/main/java/com/ingot/cloud/pms/service/SysRoleOauthClientService.java
@@ -1,6 +1,7 @@
 package com.ingot.cloud.pms.service;
 
 import com.ingot.cloud.pms.api.model.domain.SysRoleOauthClient;
+import com.ingot.framework.core.model.dto.common.RelationDto;
 import com.ingot.framework.store.mybatis.service.BaseService;
 
 /**
@@ -13,4 +14,17 @@ import com.ingot.framework.store.mybatis.service.BaseService;
  */
 public interface SysRoleOauthClientService extends BaseService<SysRoleOauthClient> {
 
+    /**
+     * 客户端绑定角色
+     *
+     * @param params 关联参数
+     */
+    void clientBindRoles(RelationDto<Long, Long> params);
+
+    /**
+     * 角色绑定客户端
+     *
+     * @param params 关联参数
+     */
+    void roleBindClients(RelationDto<Long, Long> params);
 }
--- ingot-service/ingot-pms/ingot-pms-provider/src/main/java/com/ingot/cloud/pms/service/impl/SysRoleOauthClientServiceImpl.java
+++ ingot-service/ingot-pms/ingot-pms-provider/src/main/java/com/ingot/cloud/pms/service/impl/SysRoleOauthClientServiceImpl.java
@@ -3,6 +3,7 @@ package com.ingot.cloud.pms.service.impl;
 import com.ingot.cloud.pms.api.model.domain.SysRoleOauthClient;
 import com.ingot.cloud.pms.mapper.SysRoleOauthClientMapper;
 import com.ingot.cloud.pms.service.SysRoleOauthClientService;
+import com.ingot.framework.core.model.dto.common.RelationDto;
 import com.ingot.framework.store.mybatis.service.BaseServiceImpl;
 import org.springframework.stereotype.Service;
 
@@ -17,4 +18,13 @@ import org.springframework.stereotype.Service;
 @Service
 public class SysRoleOauthClientServiceImpl extends BaseServiceImpl<SysRoleOauthClientMapper, SysRoleOauthClient> implements SysRoleOauthClientService {
 
+    @Override
+    public void clientBindRoles(RelationDto<Long, Long> params) {
+
+    }
+
+    @Override
+    public void roleBindClients(RelationDto<Long, Long> params) {
+
+    }
 }
