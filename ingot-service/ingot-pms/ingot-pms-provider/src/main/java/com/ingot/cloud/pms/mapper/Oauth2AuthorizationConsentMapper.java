52fb8a33b3a340e52d48d36a894acb5d3abccab9
wangchao <magician.of.technique@gmail.com>
2021-10-01 21:10:08 +0800

    [*] OAuth2 相关 mapper

---
+++ ingot-service/ingot-pms/ingot-pms-provider/src/main/java/com/ingot/cloud/pms/mapper/Oauth2AuthorizationConsentMapper.java
@@ -0,0 +1,16 @@
+package com.ingot.cloud.pms.mapper;
+
+import com.ingot.cloud.pms.api.model.domain.Oauth2AuthorizationConsent;
+import com.ingot.framework.store.mybatis.mapper.BaseMapper;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author jymot
+ * @since 2021-09-29
+ */
+public interface Oauth2AuthorizationConsentMapper extends BaseMapper<Oauth2AuthorizationConsent> {
+
+}
---
+++ ingot-service/ingot-pms/ingot-pms-provider/src/main/java/com/ingot/cloud/pms/mapper/Oauth2RegisteredClientMapper.java
@@ -0,0 +1,25 @@
+package com.ingot.cloud.pms.mapper;
+
+import com.ingot.cloud.pms.api.model.domain.Oauth2RegisteredClient;
+import com.ingot.framework.store.mybatis.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+/**
+ * <p>
+ * Mapper 接口
+ * </p>
+ *
+ * @author jymot
+ * @since 2021-09-29
+ */
+public interface Oauth2RegisteredClientMapper extends BaseMapper<Oauth2RegisteredClient> {
+    /**
+     * 根据角色id获取对应可用的client
+     *
+     * @param roleIds 角色ID
+     * @return Oauth2RegisteredClient 列表
+     */
+    List<Oauth2RegisteredClient> getClientsByRoles(@Param("list") List<Long> roleIds);
+}
