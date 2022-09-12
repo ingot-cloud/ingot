58007fbd7aefcdcc32a31399bb40c6e2338fbc83
wangchao <magician.of.technique@gmail.com>
2022-09-12 20:42:12 +0800

    [*] 修改类引用

--- ingot-service/ingot-auth/src/main/java/com/ingot/cloud/auth/IngotAuthApplication.java
+++ ingot-service/ingot-auth/src/main/java/com/ingot/cloud/auth/IngotAuthApplication.java
@@ -1,6 +1,6 @@
 package com.ingot.cloud.auth;
 
-import com.ingot.framework.security.config.annotation.web.configuration.EnableIngotAuthorizationServer;
+import com.ingot.framework.security.oauth2.server.authorization.config.annotation.web.configuration.EnableIngotAuthorizationServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
