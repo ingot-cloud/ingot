639f075036dff0a4c2306fbc7da03d722f01ffc0
wangchao <magician.of.technique@gmail.com>
2022-11-19 10:59:04 +0800

    [+] Test login page

---
+++ ingot-service/ingot-auth/src/main/java/com/ingot/cloud/auth/controller/LoginController.java
@@ -0,0 +1,7 @@
+package com.ingot.cloud.auth.controller;/**
+ * <p>Description  : LoginController.</p>
+ * <p>Author       : wangchao.</p>
+ * <p>Date         : 2022/11/18.</p>
+ * <p>Time         : 11:58 AM.</p>
+ */public class LoginController {
+}
