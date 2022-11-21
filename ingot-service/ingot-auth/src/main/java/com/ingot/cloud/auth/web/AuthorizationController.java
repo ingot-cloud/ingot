f7a8ceb557760eb0167aabe595b4e997f45b9962
wangchao <magician.of.technique@gmail.com>
2022-11-21 20:45:11 +0800

    [*] 测试数据

--- ingot-service/ingot-auth/src/main/java/com/ingot/cloud/auth/web/AuthorizationController.java
+++ ingot-service/ingot-auth/src/main/java/com/ingot/cloud/auth/web/AuthorizationController.java
@@ -1,8 +1,12 @@
 package com.ingot.cloud.auth.web;
 
+import javax.servlet.http.HttpServletRequest;
+
 import com.ingot.framework.security.config.annotation.web.configuration.Permit;
+import com.ingot.framework.security.core.context.ClientContextHolder;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -20,12 +24,21 @@ import org.springframework.web.servlet.ModelAndView;
 @RequestMapping("/oauth2")
 @RequiredArgsConstructor
 public class AuthorizationController {
+    private final RegisteredClientRepository registeredClientRepository;
 
     @Permit
     @GetMapping("/login")
     public ModelAndView loginPage(ModelAndView modelAndView,
+                                  HttpServletRequest request,
                                   @RequestParam(required = false) String error) {
+        log.info("login clientId={}", ClientContextHolder.get());
+        log.info("login error={}", error);
+//        RegisteredClient client = registeredClientRepository.findByClientId();
+
         modelAndView.setViewName("login/index");
+        modelAndView.addObject("error", error);
+        modelAndView.addObject("testMsg", "hahah");
+        request.setAttribute("test2", "user3");
         return modelAndView;
     }
 }
