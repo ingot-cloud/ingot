e3eff0541acee68879f4a694ff893b3abf0b77d5
wangchao <magician.of.technique@gmail.com>
2022-11-24 21:41:51 +0800

    [*] 去掉无用代码

--- authorization-view/login/src/main.ts
+++ authorization-view/login/src/main.ts
@@ -5,14 +5,12 @@ import "element-plus/dist/index.css";
 import "element-plus/theme-chalk/dark/css-vars.css";
 
 import App from "./App.vue";
-import router from "@/router";
 
 import "@/assets/main.css";
 
 const app = createApp(App);
 
 app.use(createPinia());
-app.use(router);
 app.use(ElementPlus);
 
 app.mount("#app");
