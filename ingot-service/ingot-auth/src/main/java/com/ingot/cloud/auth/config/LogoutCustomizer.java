676555629c560ce40c0350aecb68903c0b4d4466
wangchao <magician.of.technique@gmail.com>
2022-11-26 13:34:10 +0800

    [+] 增加 LogoutCustomizer

---
+++ ingot-service/ingot-auth/src/main/java/com/ingot/cloud/auth/config/LogoutCustomizer.java
@@ -0,0 +1,20 @@
+package com.ingot.cloud.auth.config;
+
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
+
+/**
+ * <p>Description  : LogoutCustomizer.</p>
+ * <p>Author       : wangchao.</p>
+ * <p>Date         : 2022/11/26.</p>
+ * <p>Time         : 12:14 PM.</p>
+ */
+public class LogoutCustomizer implements Customizer<LogoutConfigurer<HttpSecurity>> {
+
+    @Override
+    public void customize(LogoutConfigurer<HttpSecurity> configurer) {
+        configurer.deleteCookies("JSESSIONID")
+                .invalidateHttpSession(true);
+    }
+}
