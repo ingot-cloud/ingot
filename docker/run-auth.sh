645269fa67dba126c6071b6c60de0a0436afc42f
wangchao <magician.of.technique@gmail.com>
2022-12-30 09:51:03 +0800

    [*] docker run 脚本

---
+++ docker/run-auth.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+version=0.1.0
+serviceName=ingot-auth-${version}
+imageName=docker-registry.wangchao.im/ingot/auth:${version}
+
+NACOS_HOST=172.88.0.20
+NACOS_PORT=8848
+REDIS_HOST=172.88.0.90
+MYSQL_HOST=172.88.0.10
+
+INGOT_AUTH_SERVER=172.88.0.40
+INGOT_GATEWAY=172.88.0.41
+
+# 停止运行当前容器
+docker ps -q --filter name="${serviceName}" | xargs -r docker rm -f
+# 删除当前镜像
+docker images -q --filter reference="${imageName}" | xargs -r docker rmi -f
+# 拉取最新镜像
+docker pull ${imageName}
+# run
+docker run -d --name ${serviceName} --restart always \
+    --network ingot-net --ip ${INGOT_AUTH_SERVER} \
+    --add-host ingot-db-redis:${REDIS_HOST} \
+    --add-host ingot-db-mysql:${MYSQL_HOST} \
+    --add-host ingot-auth-server:${INGOT_AUTH_SERVER} \
+    --add-host ingot-gateway:${INGOT_GATEWAY} \
+    -e NACOS_HOST=${NACOS_HOST} \
+    -e NACOS_PORT=${NACOS_PORT} \
+    -e REDIS_HOST=${REDIS_HOST} \
+    -e MYSQL_HOST=${MYSQL_HOST} \
+    -v /ingot-data:/ingot-data \
+    ${imageName}
