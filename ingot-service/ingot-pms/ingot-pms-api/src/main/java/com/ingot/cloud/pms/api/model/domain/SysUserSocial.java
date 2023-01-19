43699fada02c83e09223059db7a7bfa0ab5019c8
wangchao <magician.of.technique@gmail.com>
2023-01-19 11:47:27 +0800

    [*] 提交遗漏domain

---
+++ ingot-service/ingot-pms/ingot-pms-api/src/main/java/com/ingot/cloud/pms/api/model/domain/SysUserSocial.java
@@ -0,0 +1,54 @@
+package com.ingot.cloud.pms.api.model.domain;
+
+import java.time.LocalDateTime;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import com.ingot.framework.store.mybatis.model.BaseModel;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+/**
+ * <p>
+ * 
+ * </p>
+ *
+ * @author jymot
+ * @since 2023-01-19
+ */
+@Data
+@EqualsAndHashCode(callSuper = true)
+@TableName("sys_user_social")
+public class SysUserSocial extends BaseModel<SysUserSocial> {
+
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * ID
+     */
+    @TableId(type = IdType.ASSIGN_ID)
+    private Long id;
+
+    /**
+     * 用户ID
+     */
+    private Long userId;
+
+    /**
+     * 渠道类型
+     */
+    private String type;
+
+    /**
+     * 渠道唯一ID
+     */
+    private String uniqueId;
+
+    /**
+     * 绑定时间
+     */
+    private LocalDateTime bindAt;
+
+
+}
