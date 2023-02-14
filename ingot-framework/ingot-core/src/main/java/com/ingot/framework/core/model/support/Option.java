0e9a59107d38948f69996ad2f4b6c3dcdda31cca
wangchao <magician.of.technique@gmail.com>
2023-02-14 09:37:10 +0800

    [*] 优化Option类

---
+++ ingot-framework/ingot-core/src/main/java/com/ingot/framework/core/model/support/Option.java
@@ -0,0 +1,30 @@
+package com.ingot.framework.core.model.support;
+
+import java.io.Serializable;
+
+import lombok.Data;
+
+/**
+ * <p>Description  : 选项.</p>
+ * <p>Author       : wangchao.</p>
+ * <p>Date         : 2022/12/20.</p>
+ * <p>Time         : 3:28 PM.</p>
+ */
+@Data
+public class Option<Value> implements Serializable {
+    /**
+     * 选项值
+     */
+    private Value value;
+    /**
+     * 选项名称
+     */
+    private String label;
+
+    public static <T> Option<T> of(T value, String label) {
+        Option<T> option = new Option<>();
+        option.setLabel(label);
+        option.setValue(value);
+        return option;
+    }
+}
