6dedc7cb6872aa4187715b3304ec16ef1d57d764
wangchao <magician.of.technique@gmail.com>
2023-02-14 09:15:36 +0800

    [*] 手机号登录

--- ingot-service/ingot-pms/ingot-pms-provider/src/main/java/com/ingot/cloud/pms/social/impl/PhoneSocialProcessor.java
+++ ingot-service/ingot-pms/ingot-pms-provider/src/main/java/com/ingot/cloud/pms/social/impl/PhoneSocialProcessor.java
@@ -1,7 +1,11 @@
 package com.ingot.cloud.pms.social.impl;
 
+import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import com.ingot.cloud.pms.api.model.domain.SysUser;
+import com.ingot.cloud.pms.service.domain.SysUserService;
 import com.ingot.cloud.pms.social.SocialProcessor;
+import com.ingot.framework.common.utils.DateUtils;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 import static com.ingot.framework.core.constants.SocialConstants.BEAN_PHONE;
@@ -13,20 +17,24 @@ import static com.ingot.framework.core.constants.SocialConstants.BEAN_PHONE;
  * <p>Time         : 9:55 上午.</p>
  */
 @Component(BEAN_PHONE)
+@RequiredArgsConstructor
 public class PhoneSocialProcessor implements SocialProcessor {
+    private final SysUserService sysUserService;
 
     @Override
     public String uniqueID(String code) {
-        return null;
+        return code;
     }
 
     @Override
     public SysUser info(String uniqueID) {
-        return null;
+        return sysUserService.getOne(Wrappers.<SysUser>lambdaQuery().eq(SysUser::getPhone, uniqueID));
     }
 
     @Override
     public void bind(SysUser user, String uniqueID) {
-
+        user.setPhone(uniqueID);
+        user.setUpdatedAt(DateUtils.now());
+        user.updateById();
     }
 }
