8dbca80cbb1f1412a5a330093d9b095bb57a4190
wangchao <magician.of.technique@gmail.com>
2023-02-19 14:42:34 +0800

    [*] Social API

---
+++ ingot-service/ingot-pms/ingot-pms-provider/src/main/java/com/ingot/cloud/pms/web/v1/SocialApi.java
@@ -0,0 +1,63 @@
+package com.ingot.cloud.pms.web.v1;
+
+import com.baomidou.mybatisplus.core.toolkit.Wrappers;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.ingot.cloud.pms.api.model.domain.SysSocialDetails;
+import com.ingot.cloud.pms.service.domain.SysSocialDetailsService;
+import com.ingot.framework.common.utils.DateUtils;
+import com.ingot.framework.core.model.support.R;
+import com.ingot.framework.core.model.support.RShortcuts;
+import com.ingot.framework.core.utils.validation.Group;
+import com.ingot.framework.tenant.TenantContextHolder;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * <p>Description  : SocialApi.</p>
+ * <p>Author       : wangchao.</p>
+ * <p>Date         : 2023/1/19.</p>
+ * <p>Time         : 10:19 AM.</p>
+ */
+@Slf4j
+@RestController
+@RequestMapping(value = "/v1/social")
+@RequiredArgsConstructor
+public class SocialApi implements RShortcuts {
+    private final SysSocialDetailsService sysSocialDetailsService;
+
+    @GetMapping("/page")
+    public R<?> page(Page<SysSocialDetails> page, SysSocialDetails condition) {
+        return ok(sysSocialDetailsService.page(page, Wrappers.lambdaQuery(condition)));
+    }
+
+    @PostMapping
+    public R<?> create(@RequestBody @Validated(Group.Create.class) SysSocialDetails params) {
+        params.setTenantId(TenantContextHolder.get());
+        params.setCreatedAt(DateUtils.now());
+        params.setUpdatedAt(DateUtils.now());
+        sysSocialDetailsService.save(params);
+        return ok();
+    }
+
+    @PutMapping
+    public R<?> update(@RequestBody @Validated(Group.Update.class) SysSocialDetails params) {
+        params.setUpdatedAt(DateUtils.now());
+        sysSocialDetailsService.updateById(params);
+        return ok();
+    }
+
+    @DeleteMapping("/{id}")
+    public R<?> remove(@PathVariable Long id) {
+        sysSocialDetailsService.removeById(id);
+        return ok();
+    }
+}
