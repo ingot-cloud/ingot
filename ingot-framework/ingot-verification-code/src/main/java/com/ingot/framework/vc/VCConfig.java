40e5957510b03d77a46250999c1e49eb8f20455d
wangchao <magician.of.technique@gmail.com>
2023-03-21 22:09:27 +0800

    [+] 验证码模块初始化

---
+++ ingot-framework/ingot-verification-code/src/main/java/com/ingot/framework/vc/VCConfig.java
@@ -0,0 +1,13 @@
+package com.ingot.framework.vc;
+
+import org.springframework.boot.autoconfigure.AutoConfiguration;
+
+/**
+ * <p>Description  : VCConfig.</p>
+ * <p>Author       : wangchao.</p>
+ * <p>Date         : 2023/3/21.</p>
+ * <p>Time         : 10:07 PM.</p>
+ */
+@AutoConfiguration
+public class VCConfig {
+}
