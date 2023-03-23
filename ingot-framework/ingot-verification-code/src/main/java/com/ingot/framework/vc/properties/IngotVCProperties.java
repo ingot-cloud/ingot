1c3973ec13c8732c1770eb14aacb57f5469c479c
wangchao <magician.of.technique@gmail.com>
2023-03-23 19:46:40 +0800

    [*] Update properties

--- ingot-framework/ingot-verification-code/src/main/java/com/ingot/framework/vc/properties/IngotVCProperties.java
+++ ingot-framework/ingot-verification-code/src/main/java/com/ingot/framework/vc/properties/IngotVCProperties.java
@@ -12,4 +12,16 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 @Data
 @ConfigurationProperties("ingot.vc")
 public class IngotVCProperties {
+    /**
+     * 图片验证码配置
+     */
+    private ImageCodeProperties image = new ImageCodeProperties();
+    /**
+     * 短信验证码配置
+     */
+    private SMSCodeProperties sms = new SMSCodeProperties();
+    /**
+     * 邮箱验证码配置
+     */
+    private EmailCodeProperties email = new EmailCodeProperties();
 }
