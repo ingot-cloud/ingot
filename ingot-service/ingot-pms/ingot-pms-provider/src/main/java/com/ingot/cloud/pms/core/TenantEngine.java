c7ee6ed8751077d69448aeae616b309c6cd1a5db
wangchao <magician.of.technique@gmail.com>
2023-08-01 16:49:19 +0800

    [*] 租户引擎

---
+++ ingot-service/ingot-pms/ingot-pms-provider/src/main/java/com/ingot/cloud/pms/core/TenantEngine.java
@@ -0,0 +1,14 @@
+package com.ingot.cloud.pms.core;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * <p>Description  : TenantEngine.</p>
+ * <p>Author       : wangchao.</p>
+ * <p>Date         : 2023/8/1.</p>
+ * <p>Time         : 4:25 PM.</p>
+ */
+@Component
+public class TenantEngine {
+
+}
