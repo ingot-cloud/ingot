85d504c85259e1e6b864f9f4dc5cec9314f914d0
wangchao <magician.of.technique@gmail.com>
2023-08-02 15:16:49 +0800

    [*] 增加默认角色编码

--- ingot-framework/ingot-security/src/main/java/com/ingot/framework/security/common/constants/RoleConstants.java
+++ ingot-framework/ingot-security/src/main/java/com/ingot/framework/security/common/constants/RoleConstants.java
@@ -11,4 +11,14 @@ public interface RoleConstants {
      * 超管角色编码
      */
     String ROLE_ADMIN_CODE = "role_admin";
+
+    /**
+     * 管理员角色编码
+     */
+    String ROLE_MANAGER_CODE = "role_manager";
+
+    /**
+     * 用户角色编码
+     */
+    String ROLE_USER_CODE = "role_user";
 }
