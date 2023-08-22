3eaca6d5a7deeffa2452656328f4fa8281257a20
wangchao <magician.of.technique@gmail.com>
2023-08-22 14:30:25 +0800

    [*] WebUtils 增加 hutool 方法，适配java17

--- ingot-framework/ingot-core/src/main/java/com/ingot/framework/core/utils/WebUtils.java
+++ ingot-framework/ingot-core/src/main/java/com/ingot/framework/core/utils/WebUtils.java
@@ -1,16 +1,17 @@
 package com.ingot.framework.core.utils;
 
-import java.net.InetAddress;
-import java.net.UnknownHostException;
-import java.util.Objects;
-
-import javax.servlet.http.HttpServletRequest;
-
+import cn.hutool.core.net.NetUtil;
+import cn.hutool.core.util.ArrayUtil;
 import cn.hutool.core.util.StrUtil;
 import com.ingot.framework.core.constants.GlobalConstants;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.Objects;
 
 /**
  * <p>Description  : WebUtils.</p>
@@ -86,6 +87,62 @@ public final class WebUtils extends org.springframework.web.util.WebUtils {
             }
         }
         return ipAddress;
+    }
+
+    /**
+     * 获取客户端IP
+     * 由于hutool还没更新6.0，所以没更新HttpServletRequest的包名，所以临时增加该方法
+     * <p>
+     * 默认检测的Header:
+     *
+     * <pre>
+     * 1、X-Forwarded-For
+     * 2、X-Real-IP
+     * 3、Proxy-Client-IP
+     * 4、WL-Proxy-Client-IP
+     * </pre>
+     *
+     * <p>
+     * otherHeaderNames参数用于自定义检测的Header<br>
+     * 需要注意的是，使用此方法获取的客户IP地址必须在Http服务器（例如Nginx）中配置头信息，否则容易造成IP伪造。
+     * </p>
+     *
+     * @param request          请求对象{@link HttpServletRequest}
+     * @param otherHeaderNames 其他自定义头文件，通常在Http服务器（例如Nginx）中配置
+     * @return IP地址
+     */
+    public static String getClientIP(HttpServletRequest request, String... otherHeaderNames) {
+        String[] headers = {"X-Forwarded-For", "X-Real-IP", "Proxy-Client-IP", "WL-Proxy-Client-IP", "HTTP_CLIENT_IP", "HTTP_X_FORWARDED_FOR"};
+        if (ArrayUtil.isNotEmpty(otherHeaderNames)) {
+            headers = ArrayUtil.addAll(headers, otherHeaderNames);
+        }
+
+        return getClientIPByHeader(request, headers);
+    }
+
+    /**
+     * 获取客户端IP
+     *
+     * <p>
+     * headerNames参数用于自定义检测的Header<br>
+     * 需要注意的是，使用此方法获取的客户IP地址必须在Http服务器（例如Nginx）中配置头信息，否则容易造成IP伪造。
+     * </p>
+     *
+     * @param request     请求对象{@link HttpServletRequest}
+     * @param headerNames 自定义头，通常在Http服务器（例如Nginx）中配置
+     * @return IP地址
+     */
+    public static String getClientIPByHeader(HttpServletRequest request, String... headerNames) {
+        String ip;
+        for (String header : headerNames) {
+            ip = request.getHeader(header);
+            if (!NetUtil.isUnknown(ip)) {
+                return NetUtil.getMultistageReverseProxyIp(ip);
+            }
+        }
+
+        ip = request.getRemoteAddr();
+        return NetUtil.getMultistageReverseProxyIp(ip);
     }
 
 }
