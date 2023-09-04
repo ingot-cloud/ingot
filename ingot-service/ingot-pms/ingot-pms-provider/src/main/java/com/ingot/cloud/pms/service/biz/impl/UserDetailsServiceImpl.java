abf928debf9a170a938061bbeced817c5eb92d3e
wangchao <magician.of.technique@gmail.com>
2023-09-04 10:37:51 +0800

    [*] 用户详情返回租户列表

--- ingot-service/ingot-pms/ingot-pms-provider/src/main/java/com/ingot/cloud/pms/service/biz/impl/UserDetailsServiceImpl.java
+++ ingot-service/ingot-pms/ingot-pms-provider/src/main/java/com/ingot/cloud/pms/service/biz/impl/UserDetailsServiceImpl.java
@@ -1,5 +1,6 @@
 package com.ingot.cloud.pms.service.biz.impl;
 
+import cn.hutool.core.collection.CollUtil;
 import cn.hutool.core.collection.ListUtil;
 import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import com.ingot.cloud.pms.api.model.domain.*;
@@ -7,6 +8,7 @@ import com.ingot.cloud.pms.api.model.transform.UserTrans;
 import com.ingot.cloud.pms.service.biz.UserDetailsService;
 import com.ingot.cloud.pms.service.domain.*;
 import com.ingot.cloud.pms.social.SocialProcessor;
+import com.ingot.framework.core.model.common.AllowTenantDTO;
 import com.ingot.framework.core.model.enums.CommonStatusEnum;
 import com.ingot.framework.core.model.enums.SocialTypeEnums;
 import com.ingot.framework.core.model.enums.UserStatusEnum;
@@ -36,6 +38,7 @@ public class UserDetailsServiceImpl implements UserDetailsService {
     private final SysRoleService sysRoleService;
     private final SysAuthorityService sysAuthorityService;
     private final SysTenantService sysTenantService;
+    private final SysUserTenantService sysUserTenantService;
     private final Oauth2RegisteredClientService oauth2RegisteredClientService;
     private final Map<String, SocialProcessor> socialProcessorMap;
     private final UserTrans userTrans;
@@ -69,38 +72,67 @@ public class UserDetailsServiceImpl implements UserDetailsService {
     private UserDetailsResponse map(SysUser user) {
         return Optional.ofNullable(user)
                 .map(value -> {
-                    // 获取当前登录用户租户信息，如果该租户已经被禁用，那么用户也被禁用
-                    SysTenant tenant = sysTenantService.getById(value.getTenantId());
-                    UserStatusEnum userStatus = UserStatusEnum.LOCK;
-                    if (tenant != null) {
-                        userStatus = tenant.getStatus() == CommonStatusEnum.ENABLE ?
-                                UserStatusEnum.ENABLE : UserStatusEnum.LOCK;
-                    }
+                    List<AllowTenantDTO> allows = getTenantList(user);
+
+                    UserStatusEnum userTenantStatus = CollUtil.isEmpty(allows)
+                            ? UserStatusEnum.LOCK : UserStatusEnum.ENABLE;
                     value.setStatus(value.getStatus() == UserStatusEnum.ENABLE
-                            && userStatus == UserStatusEnum.ENABLE ?
+                            && userTenantStatus == UserStatusEnum.ENABLE ?
                             UserStatusEnum.ENABLE : UserStatusEnum.LOCK);
 
                     UserDetailsResponse result = userTrans.toUserDetails(value);
+                    result.setAllows(allows);
+
                     // 查询拥有的角色
                     List<SysRole> roles = sysRoleService.getAllRolesOfUser(user.getId(), user.getDeptId());
-                    List<String> roleCodes = roles.stream()
-                            .map(SysRole::getCode)
-                            .collect(Collectors.toList());
-                    // 拥有的权限
-                    Set<String> authorities = sysAuthorityService.getAuthorityByRoles(roles)
-                            .stream().map(SysAuthority::getCode).collect(Collectors.toSet());
-                    roleCodes.addAll(authorities);
-                    result.setRoles(roleCodes);
 
-                    List<Long> roleIds = roles.stream().map(SysRole::getId).collect(Collectors.toList());
-                    // 查询可访问的客户端
-                    List<String> clientIds = Optional.ofNullable(oauth2RegisteredClientService.getClientsByRoles(roleIds))
-                            .map(clients -> clients.stream()
-                                    .map(Oauth2RegisteredClient::getClientId).collect(Collectors.toSet()))
-                            .map(ListUtil::toList)
-                            .orElse(ListUtil.toList());
-                    result.setClients(clientIds);
+                    setRoles(result, roles);
+
+                    setClients(result, roles.stream().map(SysRole::getId).collect(Collectors.toList()));
                     return result;
                 }).orElse(null);
+    }
+
+    private List<AllowTenantDTO> getTenantList(SysUser user) {
+        // 1.获取可以访问的租户列表
+        List<SysUserTenant> userTenantList = sysUserTenantService.list(
+                Wrappers.<SysUserTenant>lambdaQuery()
+                        .eq(SysUserTenant::getUserId, user.getId()));
+
+        return sysTenantService.list(
+                        Wrappers.<SysTenant>lambdaQuery()
+                                .in(SysTenant::getId, userTenantList.stream()
+                                        .map(SysUserTenant::getTenantId).collect(Collectors.toSet())))
+                .stream()
+                .filter(item -> item.getStatus() == CommonStatusEnum.ENABLE)
+                .map(item -> {
+                    AllowTenantDTO dto = new AllowTenantDTO();
+                    dto.setId(item.getId());
+                    dto.setName(item.getName());
+                    dto.setAvatar(item.getAvatar());
+                    return dto;
+                })
+                .toList();
+    }
+
+    private void setRoles(UserDetailsResponse result, List<SysRole> roles) {
+        List<String> roleCodes = roles.stream()
+                .map(SysRole::getCode)
+                .collect(Collectors.toList());
+        // 拥有的权限
+        Set<String> authorities = sysAuthorityService.getAuthorityByRoles(roles)
+                .stream().map(SysAuthority::getCode).collect(Collectors.toSet());
+        roleCodes.addAll(authorities);
+        result.setRoles(roleCodes);
+    }
+
+    private void setClients(UserDetailsResponse result, List<Long> roleIds) {
+        // 查询可访问的客户端
+        List<String> clientIds = Optional.ofNullable(oauth2RegisteredClientService.getClientsByRoles(roleIds))
+                .map(clients -> clients.stream()
+                        .map(Oauth2RegisteredClient::getClientId).collect(Collectors.toSet()))
+                .map(ListUtil::toList)
+                .orElse(ListUtil.toList());
+        result.setClients(clientIds);
     }
 }
