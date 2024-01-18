f8009936bee274cb0e5bd88446cd3e8dd99a6258
jy <magician.of.technique@aliyun.com>
2024-01-18 17:33:08 +0800

    [*] 初始化微信配置

---
+++ ingot-service/ingot-pms/ingot-pms-provider/src/main/java/com/ingot/cloud/pms/common/wechat/WechatConfiguration.java
@@ -0,0 +1,67 @@
+package com.ingot.cloud.pms.common.wechat;
+
+import cn.binarywang.wx.miniapp.api.WxMaService;
+import cn.binarywang.wx.miniapp.api.impl.WxMaServiceImpl;
+import cn.binarywang.wx.miniapp.config.WxMaConfig;
+import cn.binarywang.wx.miniapp.config.impl.WxMaDefaultConfigImpl;
+import cn.hutool.core.collection.CollUtil;
+import cn.hutool.core.util.StrUtil;
+import com.baomidou.mybatisplus.core.toolkit.Wrappers;
+import com.ingot.cloud.pms.api.model.domain.SysSocialDetails;
+import com.ingot.cloud.pms.service.domain.SysSocialDetailsService;
+import com.ingot.framework.core.model.enums.SocialTypeEnums;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * <p>Description  : WechatConfiguration.</p>
+ * <p>Author       : jy.</p>
+ * <p>Date         : 2024/1/18.</p>
+ * <p>Time         : 15:39.</p>
+ */
+@Slf4j
+@Configuration(proxyBeanMethods = false)
+@RequiredArgsConstructor
+public class WechatConfiguration implements InitializingBean {
+    private final static List<WxMaConfig> wxConfigs = new ArrayList<>();
+
+    private final SysSocialDetailsService sysSocialDetailsService;
+
+    @Bean
+    public WxMaService wxMaService() {
+        WxMaService service = new WxMaServiceImpl();
+        for (WxMaConfig config : wxConfigs) {
+            service.addConfig(config.getAppid(), config);
+        }
+        return service;
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        // 获取所有小程序配置
+        List<SysSocialDetails> list = sysSocialDetailsService.list(Wrappers.<SysSocialDetails>lambdaQuery()
+                .in(SysSocialDetails::getType, SocialTypeEnums.ADMIN_MINI_PROGRAM, SocialTypeEnums.APP_MINI_PROGRAM));
+        if (CollUtil.isEmpty(list)) {
+            return;
+        }
+
+        list.stream()
+                .map(social -> {
+                    WxMaDefaultConfigImpl config = new WxMaDefaultConfigImpl();
+                    config.setAppid(social.getAppId());
+                    config.setSecret(social.getAppSecret());
+                    return config;
+                }).forEach(config -> {
+                    if (wxConfigs.stream().anyMatch(item -> StrUtil.equals(item.getAppid(), config.getAppid()))) {
+                        return;
+                    }
+                    wxConfigs.add(config);
+                });
+    }
+}
--- ingot-service/ingot-pms/ingot-pms-provider/src/main/java/com/ingot/cloud/pms/web/v1/admin/AdminSocialAPI.java
+++ ingot-service/ingot-pms/ingot-pms-provider/src/main/java/com/ingot/cloud/pms/web/v1/admin/AdminSocialAPI.java
@@ -1,26 +1,23 @@
 package com.ingot.cloud.pms.web.v1.admin;
 
+import cn.binarywang.wx.miniapp.api.WxMaService;
+import cn.binarywang.wx.miniapp.config.impl.WxMaDefaultConfigImpl;
+import com.baomidou.mybatisplus.core.toolkit.StringUtils;
 import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.ingot.cloud.pms.api.model.domain.SysSocialDetails;
 import com.ingot.cloud.pms.service.domain.SysSocialDetailsService;
-import com.ingot.framework.core.utils.DateUtils;
 import com.ingot.framework.core.model.support.R;
 import com.ingot.framework.core.model.support.RShortcuts;
+import com.ingot.framework.core.utils.DateUtils;
 import com.ingot.framework.core.utils.validation.Group;
 import com.ingot.framework.tenant.TenantContextHolder;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * <p>Description  : SocialApi.</p>
@@ -34,6 +31,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class AdminSocialAPI implements RShortcuts {
     private final SysSocialDetailsService sysSocialDetailsService;
+    private final WxMaService wxMaService;
 
     @PreAuthorize("@ingot.hasAnyAuthority('develop.social.w', 'develop.social.r')")
     @GetMapping("/page")
@@ -43,25 +41,49 @@ public class AdminSocialAPI implements RShortcuts {
 
     @PreAuthorize("@ingot.hasAnyAuthority('develop.social.w')")
     @PostMapping
+    @Transactional(rollbackFor = Exception.class)
     public R<?> create(@RequestBody @Validated(Group.Create.class) SysSocialDetails params) {
         params.setTenantId(TenantContextHolder.get());
         params.setCreatedAt(DateUtils.now());
         params.setUpdatedAt(DateUtils.now());
         sysSocialDetailsService.save(params);
+
+        // 更新 WxMaService 配置
+        WxMaDefaultConfigImpl config = new WxMaDefaultConfigImpl();
+        config.setAppid(params.getAppId());
+        config.setSecret(params.getAppSecret());
+        wxMaService.addConfig(params.getAppId(), config);
         return ok();
     }
 
     @PreAuthorize("@ingot.hasAnyAuthority('develop.social.w')")
     @PutMapping
+    @Transactional(rollbackFor = Exception.class)
     public R<?> update(@RequestBody @Validated(Group.Update.class) SysSocialDetails params) {
         params.setUpdatedAt(DateUtils.now());
         sysSocialDetailsService.updateById(params);
+
+        // 如果更新了secret，那么需要重新add
+        if (StringUtils.isNotEmpty(params.getAppSecret())) {
+            SysSocialDetails current = sysSocialDetailsService.getById(params.getId());
+            // 更新 WxMaService 配置
+            WxMaDefaultConfigImpl config = new WxMaDefaultConfigImpl();
+            config.setAppid(current.getAppId());
+            config.setSecret(current.getAppSecret());
+            wxMaService.addConfig(current.getAppId(), config);
+        }
         return ok();
     }
 
     @PreAuthorize("@ingot.hasAnyAuthority('develop.social.w')")
     @DeleteMapping("/{id}")
+    @Transactional(rollbackFor = Exception.class)
     public R<?> remove(@PathVariable Long id) {
+
+        SysSocialDetails current = sysSocialDetailsService.getById(id);
+        if (current != null) {
+            wxMaService.removeConfig(current.getAppId());
+        }
         sysSocialDetailsService.removeById(id);
         return ok();
     }
