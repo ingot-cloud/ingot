86dac8d636407b5e5620a3bda0308738df038097
jy <magician.of.technique@aliyun.com>
2024-01-26 16:01:59 +0800

    [*] 后台app角色相关接口

--- ingot-service/ingot-pms/ingot-pms-provider/src/main/java/com/ingot/cloud/pms/service/biz/BizAppRoleService.java
+++ ingot-service/ingot-pms/ingot-pms-provider/src/main/java/com/ingot/cloud/pms/service/biz/BizAppRoleService.java
@@ -1,5 +1,8 @@
 package com.ingot.cloud.pms.service.biz;
 
+import com.ingot.cloud.pms.api.model.domain.SysRole;
+import com.ingot.cloud.pms.api.model.domain.SysRoleGroup;
+
 /**
  * <p>Description  : BizAppRoleService.</p>
  * <p>Author       : jy.</p>
@@ -7,4 +10,51 @@ package com.ingot.cloud.pms.service.biz;
  * <p>Time         : 09:30.</p>
  */
 public interface BizAppRoleService {
+    /**
+     * 创建角色，如果创建的是组织类型角色，那么会影响所有组织
+     *
+     * @param role    {@link SysRole}
+     * @param isAdmin 是否为超管
+     */
+    void createRoleEffectOrg(SysRole role, boolean isAdmin);
+
+    /**
+     * 更新角色，如果更新的是组织类型角色，那么会影响所有组织
+     *
+     * @param role    {@link SysRole}
+     * @param isAdmin 是否为超管
+     */
+    void updateRoleEffectOrg(SysRole role, boolean isAdmin);
+
+    /**
+     * 删除角色，如果删除的是组织类型角色，那么会影响所有组织
+     *
+     * @param id      角色ID
+     * @param isAdmin 是否为超管
+     */
+    void removeRoleEffectOrg(long id, boolean isAdmin);
+
+    /**
+     * 创建角色组，如果创建的是组织类型角色组，那么会影响所有组织
+     *
+     * @param group   {@link SysRoleGroup}
+     * @param isAdmin 是否为超管
+     */
+    void createRoleGroupEffectOrg(SysRoleGroup group, boolean isAdmin);
+
+    /**
+     * 更新角色组，如果更新的是组织类型角色组，那么会影响所有组织
+     *
+     * @param group   {@link SysRoleGroup}
+     * @param isAdmin 是否为超管
+     */
+    void updateRoleGroupEffectOrg(SysRoleGroup group, boolean isAdmin);
+
+    /**
+     * 删除角色组，如果删除的是组织类型角色组，那么会影响所有组织
+     *
+     * @param id      组id
+     * @param isAdmin 是否为超管
+     */
+    void removeRoleGroupEffectOrg(long id, boolean isAdmin);
 }
---
+++ ingot-service/ingot-pms/ingot-pms-provider/src/main/java/com/ingot/cloud/pms/web/v1/admin/AdminAppRoleAPI.java
@@ -0,0 +1,101 @@
+package com.ingot.cloud.pms.web.v1.admin;
+
+import com.ingot.cloud.pms.api.model.domain.AppRole;
+import com.ingot.cloud.pms.api.model.domain.SysRole;
+import com.ingot.cloud.pms.api.model.domain.SysRoleGroup;
+import com.ingot.cloud.pms.api.model.dto.role.RoleFilterDTO;
+import com.ingot.cloud.pms.api.model.dto.role.RoleGroupSortDTO;
+import com.ingot.cloud.pms.service.biz.BizAppRoleService;
+import com.ingot.cloud.pms.service.domain.AppRoleService;
+import com.ingot.framework.core.model.support.R;
+import com.ingot.framework.core.model.support.RShortcuts;
+import com.ingot.framework.core.utils.validation.Group;
+import com.ingot.framework.security.core.context.SecurityAuthContext;
+import com.ingot.framework.tenant.TenantEnv;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * <p>Description  : AdminAppRoleAPI.</p>
+ * <p>Author       : jy.</p>
+ * <p>Date         : 2024/1/23.</p>
+ * <p>Time         : 12:35.</p>
+ */
+@Slf4j
+@RestController
+@RequestMapping(value = "/v1/admin/appRole")
+@RequiredArgsConstructor
+public class AdminAppRoleAPI implements RShortcuts {
+    private final AppRoleService appRoleService;
+    private final BizAppRoleService bizAppRoleService;
+
+    @PreAuthorize("@ingot.hasAnyAuthority('app.role.w', 'app.role.r')")
+    @GetMapping("/options/{orgId}")
+    public R<?> orgOptions(@PathVariable Long orgId) {
+        return TenantEnv.applyAs(orgId, () -> ok(appRoleService.options(SecurityAuthContext.isAdmin())));
+    }
+
+    @PreAuthorize("@ingot.hasAnyAuthority('app.role.w', 'app.role.r')")
+    @GetMapping("/list")
+    public R<?> list(AppRole condition) {
+        return ok(appRoleService.conditionList(condition, SecurityAuthContext.isAdmin()));
+    }
+
+    @PreAuthorize("@ingot.hasAnyAuthority('app.role.w', 'app.role.r')")
+    @GetMapping("/group/list")
+    public R<?> groupList(RoleFilterDTO filter) {
+        return ok(appRoleService.groupRoleList(SecurityAuthContext.isAdmin(), filter));
+    }
+
+    @PreAuthorize("@ingot.hasAnyAuthority('app.role.w')")
+    @PostMapping
+    public R<?> create(@Validated(Group.Create.class) @RequestBody SysRole params) {
+        bizAppRoleService.createRoleEffectOrg(params, SecurityAuthContext.isAdmin());
+        return ok();
+    }
+
+    @PreAuthorize("@ingot.hasAnyAuthority('app.role.w')")
+    @PutMapping
+    public R<?> update(@Validated(Group.Update.class) @RequestBody SysRole params) {
+        bizAppRoleService.updateRoleEffectOrg(params, SecurityAuthContext.isAdmin());
+        return ok();
+    }
+
+    @PreAuthorize("@ingot.hasAnyAuthority('app.role.w')")
+    @DeleteMapping("/{id}")
+    public R<?> removeById(@PathVariable Long id) {
+        bizAppRoleService.removeRoleEffectOrg(id, SecurityAuthContext.isAdmin());
+        return ok();
+    }
+
+    @PreAuthorize("@ingot.hasAnyAuthority('basic.role.w')")
+    @PostMapping("/group")
+    public R<?> createGroup(@RequestBody SysRoleGroup params) {
+        bizAppRoleService.createRoleGroupEffectOrg(params, SecurityAuthContext.isAdmin());
+        return ok();
+    }
+
+    @PreAuthorize("@ingot.hasAnyAuthority('basic.role.w')")
+    @PutMapping("/group")
+    public R<?> updateGroup(@RequestBody SysRoleGroup params) {
+        bizAppRoleService.updateRoleGroupEffectOrg(params, SecurityAuthContext.isAdmin());
+        return ok();
+    }
+
+    @PreAuthorize("@ingot.hasAnyAuthority('basic.role.w')")
+    @DeleteMapping("/group/{id}")
+    public R<?> removeGroupById(@PathVariable Long id) {
+        bizAppRoleService.removeRoleGroupEffectOrg(id, SecurityAuthContext.isAdmin());
+        return ok();
+    }
+
+    @PreAuthorize("@ingot.hasAnyAuthority('basic.role.w')")
+    @PutMapping("/group/sort")
+    public R<?> groupSort(@RequestBody RoleGroupSortDTO params) {
+        appRoleService.sortGroup(params.getIds());
+        return ok();
+    }
+}
