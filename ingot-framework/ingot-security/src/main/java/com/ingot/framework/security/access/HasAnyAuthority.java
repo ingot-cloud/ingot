ad9e0aae9e8ab9e714dc23a152a870c4c80669a0
jy <magician.of.technique@aliyun.com>
2024-09-02 10:40:20 +0800

    New: 增加 IngotSecurityExpression 便捷调用注解

---
+++ ingot-framework/ingot-security/src/main/java/com/ingot/framework/security/access/HasAnyAuthority.java
@@ -0,0 +1,27 @@
+package com.ingot.framework.security.access;
+
+import com.ingot.framework.security.oauth2.server.resource.access.expression.IngotSecurityExpression;
+import org.springframework.security.access.prepost.PreAuthorize;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * <p>Description  : {@link IngotSecurityExpression#hasAnyAuthority(String...)}方法.</p>
+ * <p>Author       : jy.</p>
+ * <p>Date         : 2024/9/2.</p>
+ * <p>Time         : 10:27.</p>
+ */
+@Target({ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+@PreAuthorize("@ingot.hasAnyAuthority('{value}'.split(','))")
+public @interface HasAnyAuthority {
+    /**
+     * 权限
+     *
+     * @return {@link String[] }
+     */
+    String[] value();
+}
---
+++ ingot-framework/ingot-security/src/main/java/com/ingot/framework/security/access/RequiredAdmin.java
@@ -0,0 +1,21 @@
+package com.ingot.framework.security.access;
+
+import com.ingot.framework.security.oauth2.server.resource.access.expression.IngotSecurityExpression;
+import org.springframework.security.access.prepost.PreAuthorize;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * <p>Description  : {@link IngotSecurityExpression#requiredAdmin()}.</p>
+ * <p>Author       : jy.</p>
+ * <p>Date         : 2024/9/2.</p>
+ * <p>Time         : 10:32.</p>
+ */
+@Target({ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+@PreAuthorize("@ingot.requiredAdmin()")
+public @interface RequiredAdmin {
+}
