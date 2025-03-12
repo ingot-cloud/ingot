be83e4ae6f37598faf12f17385ec68ace39ea999
jy <magician.of.technique@aliyun.com>
2025-03-12 10:23:56 +0800

    New: 集成 com.github.yulichang:mybatis-plus-join-boot-starter

--- ingot-framework/ingot-data-mybatis/src/main/java/com/ingot/framework/data/mybatis/mapper/BaseMapper.java
+++ ingot-framework/ingot-data-mybatis/src/main/java/com/ingot/framework/data/mybatis/mapper/BaseMapper.java
@@ -1,10 +1,12 @@
 package com.ingot.framework.data.mybatis.mapper;
 
+import com.github.yulichang.base.MPJBaseMapper;
+
 /**
  * <p>Description  : BaseMapper.</p>
  * <p>Author       : wangchao.</p>
  * <p>Date         : 2020/11/13.</p>
  * <p>Time         : 6:31 下午.</p>
  */
-public interface BaseMapper<T> extends com.baomidou.mybatisplus.core.mapper.BaseMapper<T> {
+public interface BaseMapper<T> extends MPJBaseMapper<T> {
 }
