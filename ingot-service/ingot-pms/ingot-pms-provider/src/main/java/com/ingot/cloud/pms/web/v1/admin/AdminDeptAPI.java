8016f366b2809820636d46d9fb53088380ce2fe6
jy <magician.of.technique@aliyun.com>
2025-04-30 14:15:51 +0800

    Fix: admin 部门api

--- ingot-service/ingot-pms/ingot-pms-provider/src/main/java/com/ingot/cloud/pms/web/v1/admin/AdminDeptAPI.java
+++ ingot-service/ingot-pms/ingot-pms-provider/src/main/java/com/ingot/cloud/pms/web/v1/admin/AdminDeptAPI.java
@@ -1,17 +1,18 @@
 package com.ingot.cloud.pms.web.v1.admin;
 
+import com.ingot.cloud.pms.api.model.domain.SysDept;
 import com.ingot.cloud.pms.service.biz.BizDeptService;
+import com.ingot.cloud.pms.service.domain.SysDeptService;
 import com.ingot.framework.core.model.support.R;
 import com.ingot.framework.core.model.support.RShortcuts;
+import com.ingot.framework.core.utils.validation.Group;
 import com.ingot.framework.security.access.AdminOrHasAnyAuthority;
 import com.ingot.framework.tenant.TenantEnv;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * <p>Description  : AdminDeptAPI.</p>
@@ -25,11 +26,41 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/v1/admin/dept")
 @RequiredArgsConstructor
 public class AdminDeptAPI implements RShortcuts {
+    private final SysDeptService sysDeptService;
     private final BizDeptService bizDeptService;
 
     @AdminOrHasAnyAuthority({"contacts:member:r", "contacts:dept:w", "contacts:dept:r"})
     @GetMapping("/tree/{orgId}")
     public R<?> tree(@PathVariable Long orgId) {
         return TenantEnv.applyAs(orgId, () -> ok(bizDeptService.orgList()));
+    }
+
+    @AdminOrHasAnyAuthority({"orgmanager:dept"})
+    @GetMapping("/tree")
+    public R<?> tree() {
+        return ok(bizDeptService.orgList());
+    }
+
+    @AdminOrHasAnyAuthority({"orgmanager:dept"})
+    @PostMapping
+    public R<?> create(@Validated(Group.Create.class) @RequestBody SysDept params) {
+        params.setMainFlag(null);
+        sysDeptService.createDept(params);
+        return ok();
+    }
+
+    @AdminOrHasAnyAuthority({"orgmanager:dept"})
+    @PutMapping
+    public R<?> update(@Validated(Group.Update.class) @RequestBody SysDept params) {
+        params.setMainFlag(null);
+        sysDeptService.updateDept(params);
+        return ok();
+    }
+
+    @AdminOrHasAnyAuthority({"orgmanager:dept"})
+    @DeleteMapping("/{id}")
+    public R<?> removeById(@PathVariable Long id) {
+        sysDeptService.removeDeptById(id);
+        return ok();
     }
 }
