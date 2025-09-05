6eb0ff55520c343c5adbce42c96d80f2f9c6ca7f
jy <magician.of.technique@aliyun.com>
2025-09-05 08:49:16 +0800

    Fix: 优化解析，适配list

--- ingot-framework/ingot-oss-common/src/main/java/com/ingot/framework/oss/jackson/OssSaveUrlDeserializer.java
+++ ingot-framework/ingot-oss-common/src/main/java/com/ingot/framework/oss/jackson/OssSaveUrlDeserializer.java
@@ -1,14 +1,17 @@
 package com.ingot.framework.oss.jackson;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
-import cn.hutool.core.util.StrUtil;
 import com.fasterxml.jackson.core.JacksonException;
 import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.databind.DeserializationContext;
 import com.fasterxml.jackson.databind.JsonDeserializer;
+import com.fasterxml.jackson.databind.JsonNode;
 import com.ingot.framework.oss.common.OssObjectInfo;
 import com.ingot.framework.oss.common.OssPathParser;
+import lombok.extern.slf4j.Slf4j;
 
 /**
  * <p>Description  : OssSaveUrlDeserializer.</p>
@@ -16,15 +19,46 @@ import com.ingot.framework.oss.common.OssPathParser;
  * <p>Date         : 2025/9/4.</p>
  * <p>Time         : 17:29.</p>
  */
-public class OssSaveUrlDeserializer extends JsonDeserializer<String> {
+@Slf4j
+public class OssSaveUrlDeserializer extends JsonDeserializer<Object> {
 
     @Override
-    public String deserialize(JsonParser p, DeserializationContext ctxt) throws IOException, JacksonException {
-        if (p == null || StrUtil.isEmpty(p.getText())) {
+    public Object deserialize(JsonParser p, DeserializationContext ctxt) throws IOException, JacksonException {
+        if (p == null) {
             return null;
         }
+        JsonNode node = p.getCodec().readTree(p);
 
-        OssObjectInfo object = OssPathParser.parse(p.getText());
+        if (node.isArray()) {
+            List<String> result = new ArrayList<>();
+
+            // 处理数组 ["a", "b", "c"]
+            for (JsonNode element : node) {
+                String value = element.asText().trim();
+                if (!value.isEmpty()) {
+                    result.add(parseValue(value));
+                }
+            }
+
+            return result;
+        }
+
+        if (node.isTextual()) {
+            // 处理字符串
+            String text = node.asText().trim();
+
+            if (text.isEmpty()) {
+                return null;
+            }
+            return parseValue(text);
+        }
+
+        log.info("OssSaveUrlDeserializer node数据非法={}", node);
+        return null;
+    }
+
+    private String parseValue(String path) {
+        OssObjectInfo object = OssPathParser.parse(path);
         return object.bucket() + "/" + object.objectName();
     }
 }
