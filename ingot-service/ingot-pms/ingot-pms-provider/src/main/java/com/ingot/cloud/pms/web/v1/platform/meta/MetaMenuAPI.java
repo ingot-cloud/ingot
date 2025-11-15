7d3fb12c6656fb5d97a74598a8097812da9de0ff
jy <magician.of.technique@aliyun.com>
2025-11-15 15:23:30 +0800

    feat: 元数据菜单以及元数据角色API

---
+++ ingot-service/ingot-pms/ingot-pms-provider/src/main/java/com/ingot/cloud/pms/web/v1/platform/meta/MetaMenuAPI.java
@@ -0,0 +1,58 @@
+package com.ingot.cloud.pms.web.v1.platform.meta;
+
+import com.ingot.cloud.pms.api.model.domain.MetaMenu;
+import com.ingot.cloud.pms.service.biz.BizMetaMenuService;
+import com.ingot.framework.commons.model.support.R;
+import com.ingot.framework.commons.model.support.RShortcuts;
+import com.ingot.framework.core.utils.validation.Group;
+import com.ingot.framework.security.access.AdminOrHasAnyAuthority;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * <p>Description  : MetaMenuAPI.</p>
+ * <p>Author       : jy.</p>
+ * <p>Date         : 2025/11/13.</p>
+ * <p>Time         : 17:16.</p>
+ */
+@RestController
+@Tag(description = "MetaMenu", name = "元数据菜单管理模块")
+@RequestMapping(value = "/v1/platform/meta/menu")
+@RequiredArgsConstructor
+public class MetaMenuAPI implements RShortcuts {
+    private final BizMetaMenuService menuService;
+
+    @AdminOrHasAnyAuthority({"platform:menu:action", "platform:menu:read"})
+    @GetMapping("/tree")
+    @Operation(summary = "菜单树", description = "菜单树列表")
+    public R<?> tree(MetaMenu filter) {
+        return ok(menuService.treeList(filter));
+    }
+
+    @AdminOrHasAnyAuthority({"platform:menu:w"})
+    @PostMapping
+    @Operation(summary = "创建菜单", description = "创建菜单")
+    public R<?> create(@Validated(Group.Create.class) @RequestBody MetaMenu params) {
+        menuService.create(params);
+        return ok();
+    }
+
+    @AdminOrHasAnyAuthority({"platform:menu:w"})
+    @PutMapping
+    @Operation(summary = "更新菜单", description = "更新菜单")
+    public R<?> update(@Validated(Group.Update.class) @RequestBody MetaMenu params) {
+        menuService.update(params);
+        return ok();
+    }
+
+    @AdminOrHasAnyAuthority({"platform:menu:w"})
+    @DeleteMapping("/{id}")
+    @Operation(summary = "删除菜单", description = "删除菜单")
+    public R<?> removeById(@PathVariable Long id) {
+        menuService.delete(id);
+        return ok();
+    }
+}
---
+++ ingot-service/ingot-pms/ingot-pms-provider/src/main/java/com/ingot/cloud/pms/web/v1/platform/meta/MetaRoleAPI.java
@@ -0,0 +1,69 @@
+package com.ingot.cloud.pms.web.v1.platform.meta;
+
+import java.util.List;
+
+import com.ingot.cloud.pms.api.model.domain.MetaRole;
+import com.ingot.cloud.pms.api.model.vo.role.RoleItemVO;
+import com.ingot.cloud.pms.service.biz.BizMetaRoleService;
+import com.ingot.framework.commons.model.support.Option;
+import com.ingot.framework.commons.model.support.R;
+import com.ingot.framework.commons.model.support.RShortcuts;
+import com.ingot.framework.security.access.AdminOrHasAnyAuthority;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * <p>Description  : MetaRoleAPI.</p>
+ * <p>Author       : jy.</p>
+ * <p>Date         : 2025/11/12.</p>
+ * <p>Time         : 14:44.</p>
+ */
+@Slf4j
+@RestController
+@Tag(description = "MetaRole", name = "元数据角色管理模块")
+@RequestMapping(value = "/v1/platform/meta/role")
+@RequiredArgsConstructor
+public class MetaRoleAPI implements RShortcuts {
+    private final BizMetaRoleService bizMetaRoleService;
+
+    @AdminOrHasAnyAuthority({"platform:role:query"})
+    @GetMapping(value = "/options")
+    @Operation(summary = "角色选项", description = "角色选项列表")
+    public R<List<Option<Long>>> options(MetaRole condition) {
+        return ok(bizMetaRoleService.options(condition));
+    }
+
+    @AdminOrHasAnyAuthority({"platform:role:query"})
+    @GetMapping(value = "/list")
+    @Operation(summary = "角色列表", description = "角色列表")
+    public R<List<RoleItemVO>> conditionList(MetaRole condition) {
+        return ok(bizMetaRoleService.conditionList(condition));
+    }
+
+    @AdminOrHasAnyAuthority({"platform:role:create"})
+    @PostMapping
+    @Operation(summary = "创建角色", description = "创建角色")
+    public R<Void> create(@RequestBody MetaRole params) {
+        bizMetaRoleService.create(params);
+        return ok();
+    }
+
+    @AdminOrHasAnyAuthority({"platform:role:update"})
+    @PutMapping
+    @Operation(summary = "更新角色", description = "更新角色")
+    public R<Void> update(@RequestBody MetaRole params) {
+        bizMetaRoleService.update(params);
+        return ok();
+    }
+
+    @AdminOrHasAnyAuthority({"platform:role:delete"})
+    @DeleteMapping(value = "/{id}")
+    @Operation(summary = "删除角色", description = "删除角色")
+    public R<Void> delete(@PathVariable Long id) {
+        bizMetaRoleService.delete(id);
+        return ok();
+    }
+}
