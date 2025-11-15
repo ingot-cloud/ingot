8be2774abfea1fb4725891861763495d5794eb69
jy <magician.of.technique@aliyun.com>
2025-11-15 16:01:14 +0800

    feat: 元数据权限接口

---
+++ ingot-service/ingot-pms/ingot-pms-provider/src/main/java/com/ingot/cloud/pms/web/v1/platform/meta/MetaAuthorityAPI.java
@@ -0,0 +1,61 @@
+package com.ingot.cloud.pms.web.v1.platform.meta;
+
+import java.util.List;
+
+import com.ingot.cloud.pms.api.model.domain.MetaAuthority;
+import com.ingot.cloud.pms.api.model.vo.authority.AuthorityTreeNodeVO;
+import com.ingot.cloud.pms.service.biz.BizMetaAuthorityService;
+import com.ingot.framework.commons.model.support.R;
+import com.ingot.framework.commons.model.support.RShortcuts;
+import com.ingot.framework.core.utils.validation.Group;
+import com.ingot.framework.security.access.AdminOrHasAnyAuthority;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * <p>Description  : MetaAuthorityAPI.</p>
+ * <p>Author       : jy.</p>
+ * <p>Date         : 2025/11/15.</p>
+ * <p>Time         : 15:50.</p>
+ */
+@RestController
+@Tag(description = "MetaAuthority", name = "元数据菜单管理模块")
+@RequestMapping(value = "/v1/platform/meta/authority")
+@RequiredArgsConstructor
+public class MetaAuthorityAPI implements RShortcuts {
+    private final BizMetaAuthorityService metaAuthorityService;
+
+    @AdminOrHasAnyAuthority({"meta:authority:query"})
+    @GetMapping("/tree")
+    @Operation(summary = "权限树", description = "权限树列表")
+    public R<List<AuthorityTreeNodeVO>> tree(MetaAuthority params) {
+        return ok(metaAuthorityService.treeList(params));
+    }
+
+    @AdminOrHasAnyAuthority({"meta:authority:create"})
+    @PostMapping
+    @Operation(summary = "创建权限", description = "创建权限")
+    public R<?> create(@Validated(Group.Create.class) @RequestBody MetaAuthority params) {
+        metaAuthorityService.createNonMenuAuthority(params);
+        return ok();
+    }
+
+    @AdminOrHasAnyAuthority({"meta:authority:update"})
+    @PutMapping
+    @Operation(summary = "更新权限", description = "更新权限")
+    public R<?> update(@Validated(Group.Update.class) @RequestBody MetaAuthority params) {
+        metaAuthorityService.updateNonMenuAuthority(params);
+        return ok();
+    }
+
+    @AdminOrHasAnyAuthority({"meta:authority:delete"})
+    @DeleteMapping("/{id}")
+    @Operation(summary = "删除权限", description = "删除权限")
+    public R<?> removeById(@PathVariable Long id) {
+        metaAuthorityService.deleteNonMenuAuthority(id);
+        return ok();
+    }
+}
