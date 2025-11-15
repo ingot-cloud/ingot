db0e5caaef0e672ee2d43292dc7d30f44f6690d0
jy <magician.of.technique@aliyun.com>
2025-11-15 15:11:23 +0800

    refactor(type): 权限角色类型
    1. 优化AuthorityType
    2. 优化RoleType

--- ingot-service/ingot-pms/ingot-pms-api/src/main/java/com/ingot/cloud/pms/api/model/types/AuthorityType.java
+++ ingot-service/ingot-pms/ingot-pms-api/src/main/java/com/ingot/cloud/pms/api/model/types/AuthorityType.java
@@ -1,5 +1,6 @@
 package com.ingot.cloud.pms.api.model.types;
 
+import com.ingot.cloud.pms.api.model.enums.AuthorityTypeEnum;
 import com.ingot.cloud.pms.api.model.enums.OrgTypeEnum;
 import com.ingot.framework.commons.model.enums.CommonStatusEnum;
 
@@ -10,30 +11,58 @@ import com.ingot.framework.commons.model.enums.CommonStatusEnum;
  * <p>Time         : 10:49.</p>
  */
 public interface AuthorityType {
+    /**
+     * ID
+     */
     Long getId();
 
     void setId(Long id);
 
+    /**
+     * PID
+     */
     Long getPid();
 
     void setPid(Long pid);
 
+    /**
+     * 权限名称
+     */
     String getName();
 
     void setName(String name);
 
+    /**
+     * 权限编码
+     */
     String getCode();
 
     void setCode(String code);
 
+    /**
+     * 权限类型
+     */
+    AuthorityTypeEnum getType();
+
+    void setType(AuthorityTypeEnum type);
+
+    /**
+     * 组织类型
+     */
+    OrgTypeEnum getOrgType();
+
+    void setOrgType(OrgTypeEnum type);
+
+    /**
+     * 状态, 0:正常，9:禁用
+     */
     CommonStatusEnum getStatus();
 
     void setStatus(CommonStatusEnum status);
 
-    OrgTypeEnum getType();
-
-    void setOrgType(OrgTypeEnum type);
-
+    /**
+     * 备注
+     */
     String getRemark();
 
     void setRemark(String remark);
--- ingot-service/ingot-pms/ingot-pms-api/src/main/java/com/ingot/cloud/pms/api/model/types/RoleType.java
+++ ingot-service/ingot-pms/ingot-pms-api/src/main/java/com/ingot/cloud/pms/api/model/types/RoleType.java
@@ -1,7 +1,11 @@
 package com.ingot.cloud.pms.api.model.types;
 
+import java.util.List;
+
 import com.ingot.cloud.pms.api.model.enums.OrgTypeEnum;
+import com.ingot.cloud.pms.api.model.enums.RoleTypeEnum;
 import com.ingot.framework.commons.model.enums.CommonStatusEnum;
+import com.ingot.framework.data.mybatis.common.model.DataScopeTypeEnum;
 
 /**
  * <p>Description  : RoleType.</p>
@@ -15,38 +19,61 @@ public interface RoleType {
      */
     Long getId();
 
-    /**
-     * 租户
-     */
-    Long getTenantId();
+    void setId(Long id);
 
     /**
-     * 模型ID
+     * PID
      */
-    Long getModelId();
+    Long getPid();
 
-    /**
-     * 组ID
-     */
-    Long getGroupId();
+    void setPid(Long id);
 
     /**
      * 角色名称
      */
     String getName();
 
+    void setName(String name);
+
     /**
      * 角色编码
      */
     String getCode();
 
+    void setCode(String code);
+
     /**
      * 角色类型
      */
-    OrgTypeEnum getType();
+    RoleTypeEnum getType();
+
+    void setType(RoleTypeEnum type);
+
+    /**
+     * 组织类型
+     */
+    OrgTypeEnum getOrgType();
+
+    void setOrgType(OrgTypeEnum orgType);
+
+    /**
+     * 域类型
+     */
+    DataScopeTypeEnum getScopeType();
+
+    void setScopeType(DataScopeTypeEnum scopeType);
+
+    /**
+     * Scopes
+     */
+    List<Long> getScopes();
+
+    void setScopes(List<Long> scopes);
 
     /**
      * 状态, 0:正常，9:禁用
      */
     CommonStatusEnum getStatus();
+
+    void setStatus(CommonStatusEnum status);
 }
