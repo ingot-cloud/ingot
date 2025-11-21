7da2b9b0a5ab6a0a1a41bb55b002e35ba66619c1
jy <magician.of.technique@aliyun.com>
2025-11-21 16:23:10 +0800

    refactor: 权限API TAG

--- ingot-service/ingot-pms/ingot-pms-provider/src/main/java/com/ingot/cloud/pms/web/v1/org/OrgAuthAPI.java
+++ ingot-service/ingot-pms/ingot-pms-provider/src/main/java/com/ingot/cloud/pms/web/v1/org/OrgAuthAPI.java
@@ -1,17 +1,17 @@
 package com.ingot.cloud.pms.web.v1.org;
 
-import com.ingot.cloud.pms.api.model.dto.authority.AuthorityFilterDTO;
-import com.ingot.cloud.pms.service.biz.BizRoleService;
-import com.ingot.framework.commons.model.common.RelationDTO;
+import com.ingot.cloud.pms.service.biz.BizOrgService;
 import com.ingot.framework.commons.model.support.R;
 import com.ingot.framework.commons.model.support.RShortcuts;
 import com.ingot.framework.security.access.AdminOrHasAnyAuthority;
 import com.ingot.framework.tenant.TenantContextHolder;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * <p>Description  : OrgAuthAPI.</p>
@@ -20,30 +20,17 @@ import org.springframework.web.bind.annotation.*;
  * <p>Time         : 14:55.</p>
  */
 @Slf4j
-@Tag(description = "orgAuth", name = "组织权限模块")
+@Tag(description = "OrgAuth", name = "组织权限模块")
 @RestController
 @RequestMapping(value = "/v1/org/auth")
 @RequiredArgsConstructor
 public class OrgAuthAPI implements RShortcuts {
-    private final BizRoleService bizRoleService;
+    private final BizOrgService bizOrgService;
 
-    @AdminOrHasAnyAuthority("contacts:auth")
-    @GetMapping("/list")
-    public R<?> getOrgAuth() {
-        return ok(bizRoleService.getOrgAuthority(TenantContextHolder.get()));
-    }
-
-    @AdminOrHasAnyAuthority("contacts:auth")
-    @PutMapping("/bindAuthority")
-    public R<?> bindAuthority(@RequestBody @Validated RelationDTO<Long, Long> params) {
-        bizRoleService.orgRoleBindAuthorities(params);
-        return ok();
-    }
-
-    @AdminOrHasAnyAuthority("contacts:auth")
-    @GetMapping("/bindAuthority/{id}")
-    public R<?> getBindAuthorities(@PathVariable Long id,
-                                   AuthorityFilterDTO condition) {
-        return ok(bizRoleService.getOrgRoleAuthorities(id, condition));
+    @Operation(summary = "获取组织权限", description = "获取组织权限")
+    @AdminOrHasAnyAuthority("contacts:auth:query")
+    @GetMapping("/tree")
+    public R<?> getOrgAuthTree() {
+        return ok(bizOrgService.getTenantAuthorityTree(TenantContextHolder.get()));
     }
 }
