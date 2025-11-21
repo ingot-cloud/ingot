fa7c85f46574ee73123d1d406e0f0b0cc43e3162
jy <magician.of.technique@aliyun.com>
2025-11-21 13:47:34 +0800

    refactor: 优化组织角色api

--- ingot-service/ingot-pms/ingot-pms-provider/src/main/java/com/ingot/cloud/pms/web/v1/org/OrgRoleAPI.java
+++ ingot-service/ingot-pms/ingot-pms-provider/src/main/java/com/ingot/cloud/pms/web/v1/org/OrgRoleAPI.java
@@ -1,21 +1,16 @@
 package com.ingot.cloud.pms.web.v1.org;
 
-import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
-import com.ingot.cloud.pms.api.model.domain.SysRole;
-import com.ingot.cloud.pms.api.model.domain.SysRoleGroup;
-import com.ingot.cloud.pms.api.model.dto.authority.AuthorityFilterDTO;
-import com.ingot.cloud.pms.api.model.dto.role.RoleFilterDTO;
-import com.ingot.cloud.pms.api.model.dto.role.RoleGroupSortDTO;
+import com.ingot.cloud.pms.api.model.domain.MetaAuthority;
+import com.ingot.cloud.pms.api.model.domain.TenantRolePrivate;
+import com.ingot.cloud.pms.api.model.dto.role.BizRoleAssignUsersDTO;
 import com.ingot.cloud.pms.api.model.enums.OrgTypeEnum;
 import com.ingot.cloud.pms.service.biz.BizRoleService;
-import com.ingot.cloud.pms.service.domain.SysAuthorityService;
-import com.ingot.cloud.pms.service.domain.SysRoleAuthorityService;
-import com.ingot.cloud.pms.service.domain.SysRoleService;
-import com.ingot.framework.commons.model.common.RelationDTO;
+import com.ingot.framework.commons.model.common.AssignDTO;
 import com.ingot.framework.commons.model.support.R;
 import com.ingot.framework.commons.model.support.RShortcuts;
 import com.ingot.framework.core.utils.validation.Group;
 import com.ingot.framework.security.access.AdminOrHasAnyAuthority;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -29,117 +24,78 @@ import org.springframework.web.bind.annotation.*;
  * <p>Time         : 2:40 PM.</p>
  */
 @Slf4j
-@Tag(description = "orgRole", name = "组织角色模块")
+@Tag(description = "OrgRole", name = "组织角色模块")
 @RestController
 @RequestMapping(value = "/v1/org/role")
 @RequiredArgsConstructor
 public class OrgRoleAPI implements RShortcuts {
-    private final SysRoleService sysRoleService;
-    private final SysAuthorityService sysAuthorityService;
-    private final SysRoleAuthorityService sysRoleAuthorityService;
     private final BizRoleService bizRoleService;
 
-    @AdminOrHasAnyAuthority({"contacts:role:w", "contacts:role:r"})
+    @Operation(summary = "角色选项", description = "角色选项")
+    @AdminOrHasAnyAuthority({"contacts:role:query"})
     @GetMapping("/options")
     public R<?> options() {
-        return ok(sysRoleService.options(false));
+        return ok(bizRoleService.options(null));
     }
 
-    @AdminOrHasAnyAuthority({"contacts:role:w", "contacts:role:r"})
-    @GetMapping("/list")
-    public R<?> list(SysRole condition) {
-        return ok(sysRoleService.conditionList(condition, false));
+    @Operation(summary = "角色树", description = "角色树")
+    @AdminOrHasAnyAuthority({"contacts:role:query"})
+    @GetMapping("/tree")
+    public R<?> list(TenantRolePrivate condition) {
+        return ok(bizRoleService.conditionTree(condition));
     }
 
-    @AdminOrHasAnyAuthority({"contacts:role:w", "contacts:role:r"})
-    @GetMapping("/page")
-    public R<?> page(Page<SysRole> page, SysRole condition) {
-        return ok(sysRoleService.conditionPage(page, condition, false));
-    }
-
-    @AdminOrHasAnyAuthority({"contacts:role:w", "contacts:role:r"})
-    @GetMapping("/group/list")
-    public R<?> groupList(RoleFilterDTO filter) {
-        return ok(sysRoleService.groupRoleList(false, filter));
-    }
-
-    @AdminOrHasAnyAuthority({"contacts:role:w"})
+    @Operation(summary = "创建角色", description = "创建角色")
+    @AdminOrHasAnyAuthority({"contacts:role:create"})
     @PostMapping
-    public R<?> create(@Validated(Group.Create.class) @RequestBody SysRole params) {
-        // 组织创建角色，角色不能为部门角色
-        params.setFilterDept(Boolean.FALSE);
-        // 自定义组织角色，类型为CUSTOM
-        params.setType(OrgTypeEnum.Custom);
-        sysRoleService.createRole(params, false);
+    public R<?> create(@Validated(Group.Create.class) @RequestBody TenantRolePrivate params) {
+        // 自定义组织角色
+        params.setOrgType(OrgTypeEnum.Tenant);
+        bizRoleService.create(params);
         return ok();
     }
 
-    @AdminOrHasAnyAuthority({"contacts:role:w"})
+    @Operation(summary = "更新角色", description = "更新角色")
+    @AdminOrHasAnyAuthority({"contacts:role:update"})
     @PutMapping
-    public R<?> update(@Validated(Group.Update.class) @RequestBody SysRole params) {
-        // 组织更新角色，角色不能更新为部门角色
-        params.setFilterDept(null);
-        sysRoleService.updateRoleById(params, false);
+    public R<?> update(@Validated(Group.Update.class) @RequestBody TenantRolePrivate params) {
+        bizRoleService.update(params);
         return ok();
     }
 
-    @AdminOrHasAnyAuthority({"contacts:role:w"})
+    @Operation(summary = "删除角色", description = "删除角色")
+    @AdminOrHasAnyAuthority({"contacts:role:delete"})
     @DeleteMapping("/{id}")
     public R<?> removeById(@PathVariable Long id) {
-        sysRoleService.removeRoleById(id, false);
+        bizRoleService.delete(id);
         return ok();
     }
 
-    @AdminOrHasAnyAuthority({"contacts:role:w"})
-    @PostMapping("/group")
-    public R<?> createGroup(@RequestBody SysRoleGroup params) {
-        sysRoleService.createGroup(params, false);
+    @Operation(summary = "角色分配用户", description = "角色分配用户")
+    @AdminOrHasAnyAuthority({"contacts:role:user:assign"})
+    @PutMapping("/{id}/users")
+    public R<?> assignUsers(@PathVariable Long id,
+                            @RequestBody BizRoleAssignUsersDTO params) {
+        params.setId(id);
+        bizRoleService.assignUsers(params);
         return ok();
     }
 
-    @AdminOrHasAnyAuthority({"contacts:role:w"})
-    @PutMapping("/group")
-    public R<?> updateGroup(@RequestBody SysRoleGroup params) {
-        sysRoleService.updateGroup(params, false);
+    @Operation(summary = "设置角色权限", description = "设置角色权限")
+    @AdminOrHasAnyAuthority("contacts:role:authorities:set")
+    @PutMapping("/{id}/authorities")
+    public R<?> setAuthorities(@PathVariable Long id,
+                               @RequestBody AssignDTO<Long, Long> params) {
+        params.setId(id);
+        bizRoleService.setAuthorities(params);
         return ok();
     }
 
-    @AdminOrHasAnyAuthority({"contacts:role:w"})
-    @DeleteMapping("/group/{id}")
-    public R<?> removeGroupById(@PathVariable Long id) {
-        sysRoleService.deleteGroup(id, false);
-        return ok();
-    }
-
-    @AdminOrHasAnyAuthority({"contacts:role:w"})
-    @PutMapping("/group/sort")
-    public R<?> groupSort(@RequestBody RoleGroupSortDTO params) {
-        sysRoleService.sortGroup(params.getIds());
-        return ok();
-    }
-
-    @AdminOrHasAnyAuthority({"contacts:role:w"})
-    @PutMapping("/bindAuthority")
-    public R<?> bindAuthority(@RequestBody @Validated RelationDTO<Long, Long> params) {
-        sysRoleAuthorityService.roleBindAuthorities(params);
-        return ok();
-    }
-
-    @AdminOrHasAnyAuthority({"contacts:role:w"})
-    @GetMapping("/bindAuthority/{id}")
-    public R<?> getBindAuthorities(@PathVariable Long id,
-                                   @RequestParam("isBind") boolean isBind,
-                                   AuthorityFilterDTO condition) {
-        if (isBind) {
-            return ok(sysRoleAuthorityService.getRoleAuthorities(id, condition));
-        }
-        return ok(sysAuthorityService.treeList(condition));
-    }
-
-    @AdminOrHasAnyAuthority({"contacts:role:w"})
-    @PutMapping("/bindUser")
-    public R<?> bindUser(@RequestBody @Validated RelationDTO<Long, Long> params) {
-        bizRoleService.orgRoleBindUsers(params);
-        return ok();
+    @Operation(summary = "获取角色权限树", description = "获取角色权限树")
+    @AdminOrHasAnyAuthority("contacts:role:authorities:query")
+    @GetMapping("/{id}/authorities")
+    public R<?> getRoleAuthoritiesTree(@PathVariable Long id,
+                                       MetaAuthority condition) {
+        return ok(bizRoleService.getRoleAuthoritiesTree(id, condition));
     }
 }
