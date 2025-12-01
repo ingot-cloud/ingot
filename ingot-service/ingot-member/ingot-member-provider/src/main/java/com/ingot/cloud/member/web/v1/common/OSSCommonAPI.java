ca2b57f09540fcf60d10572b96d9ca1c31e88058
jy <magician.of.technique@aliyun.com>
2025-12-01 14:22:16 +0800

    refactor: OSS

---
+++ ingot-service/ingot-member/ingot-member-provider/src/main/java/com/ingot/cloud/member/web/v1/common/OSSCommonAPI.java
@@ -0,0 +1,42 @@
+package com.ingot.cloud.member.web.v1.common;
+
+import java.io.IOException;
+
+import com.ingot.framework.commons.error.IllegalOperationException;
+import com.ingot.framework.commons.model.support.R;
+import com.ingot.framework.commons.model.support.RShortcuts;
+import com.ingot.framework.commons.oss.OssService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+/**
+ * <p>Description  : OSSCommonAPI.</p>
+ * <p>Author       : wangchao.</p>
+ * <p>Date         : 2023/8/7.</p>
+ * <p>Time         : 10:13 AM.</p>
+ */
+@RestController
+@Tag(description = "OSS", name = "OSS模块")
+@RequestMapping(value = "/v1/oss")
+@RequiredArgsConstructor
+public class OSSCommonAPI implements RShortcuts {
+    private final OssService ossService;
+
+    @Operation(summary = "上传文件", description = "上传文件")
+    @PostMapping("/upload")
+    public R<?> upload(@RequestParam("file") MultipartFile file,
+                       @RequestParam(value = "bucketName") String bucketName,
+                       @RequestParam(value = "fileName") String fileName) {
+        try {
+            return ok(ossService.uploadFile(bucketName, fileName, file.getInputStream()));
+        } catch (IOException e) {
+            throw new IllegalOperationException("上传失败");
+        }
+    }
+}
