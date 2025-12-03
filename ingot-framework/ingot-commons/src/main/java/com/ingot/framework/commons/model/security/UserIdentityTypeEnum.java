67d53c76dac005a8d111bd70ee70039999469ad2
jy <magician.of.technique@aliyun.com>
2025-12-03 17:38:10 +0800

    feat: 用户身份枚举

---
+++ ingot-framework/ingot-commons/src/main/java/com/ingot/framework/commons/model/security/UserIdentityTypeEnum.java
@@ -0,0 +1,18 @@
+package com.ingot.framework.commons.model.security;
+
+/**
+ * <p>Description  : 身份类型.</p>
+ * <p>Author       : jy.</p>
+ * <p>Date         : 2025/12/1.</p>
+ * <p>Time         : 17:38.</p>
+ */
+public enum UserIdentityTypeEnum {
+    /**
+     * 用户名，包括用户名，手机号，邮箱
+     */
+    USERNAME,
+    /**
+     * 社交登录
+     */
+    SOCIAL
+}
