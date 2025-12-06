74d891bc106506fab8369c71e078b9f8c55daca6
jy <magician.of.technique@aliyun.com>
2025-12-06 10:50:36 +0800

    chore: ingot_member.sql

---
+++ databases/ingot_member.sql
@@ -0,0 +1,227 @@
+/*
+ Navicat Premium Dump SQL
+
+ Source Server         : localhost
+ Source Server Type    : MySQL
+ Source Server Version : 80032 (8.0.32)
+ Source Host           : localhost:3306
+ Source Schema         : ingot_member
+
+ Target Server Type    : MySQL
+ Target Server Version : 80032 (8.0.32)
+ File Encoding         : 65001
+
+ Date: 05/12/2025 18:13:00
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for biz_leaf_alloc
+-- ----------------------------
+DROP TABLE IF EXISTS `biz_leaf_alloc`;
+CREATE TABLE `biz_leaf_alloc` (
+  `biz_tag` varchar(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL DEFAULT '',
+  `max_id` bigint NOT NULL DEFAULT '1',
+  `step` int NOT NULL,
+  `description` varchar(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `update_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`biz_tag`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- ----------------------------
+-- Records of biz_leaf_alloc
+-- ----------------------------
+BEGIN;
+COMMIT;
+
+-- ----------------------------
+-- Table structure for member_permission
+-- ----------------------------
+DROP TABLE IF EXISTS `member_permission`;
+CREATE TABLE `member_permission` (
+  `id` bigint unsigned NOT NULL COMMENT 'ID',
+  `pid` bigint unsigned NOT NULL DEFAULT '0' COMMENT '父ID',
+  `name` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '权限名称',
+  `code` varchar(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '权限编码',
+  `type` char(1) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '类型',
+  `status` char(1) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL DEFAULT '0' COMMENT '状态, 0:正常，9:禁用',
+  `remark` varchar(255) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci DEFAULT '' COMMENT '备注',
+  `created_at` datetime DEFAULT NULL COMMENT '创建日期',
+  `updated_at` datetime DEFAULT NULL COMMENT '更新日期',
+  `deleted_at` datetime DEFAULT NULL COMMENT '删除日期',
+  PRIMARY KEY (`id`),
+  KEY `idx_code` (`code`) USING BTREE COMMENT '编码'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- ----------------------------
+-- Records of member_permission
+-- ----------------------------
+BEGIN;
+COMMIT;
+
+-- ----------------------------
+-- Table structure for member_role
+-- ----------------------------
+DROP TABLE IF EXISTS `member_role`;
+CREATE TABLE `member_role` (
+  `id` bigint unsigned NOT NULL COMMENT 'ID',
+  `pid` bigint NOT NULL DEFAULT '0' COMMENT '组ID',
+  `name` varchar(50) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL DEFAULT '' COMMENT '角色名称',
+  `code` varchar(50) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL DEFAULT '' COMMENT '角色编码',
+  `built_in` tinyint(1) NOT NULL DEFAULT '0' COMMENT '是否内置',
+  `status` char(1) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL DEFAULT '0' COMMENT '状态, 0:正常，9:禁用',
+  `created_at` datetime DEFAULT NULL COMMENT '创建日期',
+  `updated_at` datetime DEFAULT NULL COMMENT '更新日期',
+  `deleted_at` datetime DEFAULT NULL COMMENT '删除日期',
+  PRIMARY KEY (`id`),
+  KEY `idx_code` (`code`) USING BTREE COMMENT '编码'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- ----------------------------
+-- Records of member_role
+-- ----------------------------
+BEGIN;
+INSERT INTO `member_role` (`id`, `pid`, `name`, `code`, `built_in`, `status`, `created_at`, `updated_at`, `deleted_at`) VALUES (1175083532130185217, 0, '用户角色', 'role_member_user', 1, '0', '2025-12-05 14:28:27', '2025-12-05 14:28:27', NULL);
+COMMIT;
+
+-- ----------------------------
+-- Table structure for member_role_permission
+-- ----------------------------
+DROP TABLE IF EXISTS `member_role_permission`;
+CREATE TABLE `member_role_permission` (
+  `id` bigint NOT NULL COMMENT 'ID',
+  `tenant_id` bigint NOT NULL COMMENT '租户ID',
+  `role_id` bigint unsigned NOT NULL COMMENT '角色ID',
+  `permission_id` bigint unsigned NOT NULL COMMENT '权限ID',
+  PRIMARY KEY (`id`),
+  KEY `idx_tenant_role` (`role_id`,`tenant_id`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- ----------------------------
+-- Records of member_role_permission
+-- ----------------------------
+BEGIN;
+COMMIT;
+
+-- ----------------------------
+-- Table structure for member_role_user
+-- ----------------------------
+DROP TABLE IF EXISTS `member_role_user`;
+CREATE TABLE `member_role_user` (
+  `id` bigint NOT NULL COMMENT 'ID',
+  `role_id` bigint unsigned NOT NULL COMMENT '角色ID',
+  `user_id` bigint unsigned NOT NULL COMMENT '用户ID',
+  `tenant_id` bigint NOT NULL COMMENT '租户ID',
+  PRIMARY KEY (`id`),
+  KEY `idx_user` (`user_id`) USING BTREE,
+  KEY `idx_role` (`role_id`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- ----------------------------
+-- Records of member_role_user
+-- ----------------------------
+BEGIN;
+COMMIT;
+
+-- ----------------------------
+-- Table structure for member_social_details
+-- ----------------------------
+DROP TABLE IF EXISTS `member_social_details`;
+CREATE TABLE `member_social_details` (
+  `id` bigint unsigned NOT NULL COMMENT 'ID',
+  `tenant_id` bigint unsigned NOT NULL COMMENT '租户ID',
+  `app_id` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT 'App ID',
+  `app_secret` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL COMMENT 'App Secret',
+  `redirect_url` varchar(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL COMMENT '重定向地址',
+  `name` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL COMMENT '社交名称',
+  `type` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL COMMENT '类型',
+  `status` char(1) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci DEFAULT '0' COMMENT '状态, 0:正常，9:禁用',
+  `created_at` datetime DEFAULT NULL COMMENT '创建日期',
+  `updated_at` datetime DEFAULT NULL COMMENT '更新日期',
+  `deleted_at` datetime DEFAULT NULL COMMENT '删除日期',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- ----------------------------
+-- Records of member_social_details
+-- ----------------------------
+BEGIN;
+COMMIT;
+
+-- ----------------------------
+-- Table structure for member_user
+-- ----------------------------
+DROP TABLE IF EXISTS `member_user`;
+CREATE TABLE `member_user` (
+  `id` bigint unsigned NOT NULL COMMENT 'ID',
+  `username` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL COMMENT '用户名',
+  `password` varchar(300) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL COMMENT '密码',
+  `init_pwd` tinyint(1) NOT NULL DEFAULT '1' COMMENT '初始化密码标识',
+  `nickname` varchar(32) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci DEFAULT NULL COMMENT '昵称',
+  `phone` varchar(32) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci DEFAULT NULL COMMENT '手机号',
+  `email` varchar(64) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci DEFAULT NULL COMMENT '邮件地址',
+  `avatar` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL COMMENT '头像',
+  `status` char(1) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL DEFAULT '0' COMMENT '状态, 0:正常，9:禁用',
+  `created_at` datetime DEFAULT NULL COMMENT '创建日期',
+  `updated_at` datetime DEFAULT NULL COMMENT '更新日期',
+  `deleted_at` datetime DEFAULT NULL COMMENT '删除日期',
+  PRIMARY KEY (`id`),
+  KEY `idx_username` (`username`) USING BTREE COMMENT '用户名',
+  KEY `idx_phone` (`phone`) USING BTREE COMMENT '手机号',
+  KEY `idx_email` (`email`) USING BTREE COMMENT '邮箱'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- ----------------------------
+-- Records of member_user
+-- ----------------------------
+BEGIN;
+INSERT INTO `member_user` (`id`, `username`, `password`, `init_pwd`, `nickname`, `phone`, `email`, `avatar`, `status`, `created_at`, `updated_at`, `deleted_at`) VALUES (1175075664995807234, '18612341234', '{bcrypt}$2a$10$Io6Sn3JN4MYrnqvLUoNpzeckRk9kkgymJLpRozuA8Ve9taCINbaYG', 1, '测试用户', '18612341234', NULL, 'ingot/user/avatar/ic_logo.png', '0', '2025-12-05 13:57:11', '2025-12-05 14:28:05', NULL);
+COMMIT;
+
+-- ----------------------------
+-- Table structure for member_user_social
+-- ----------------------------
+DROP TABLE IF EXISTS `member_user_social`;
+CREATE TABLE `member_user_social` (
+  `id` bigint NOT NULL COMMENT 'ID',
+  `tenant_id` bigint NOT NULL COMMENT '组织ID',
+  `user_id` bigint NOT NULL COMMENT '用户ID',
+  `type` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '渠道类型',
+  `unique_id` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '渠道唯一ID',
+  `bind_at` datetime NOT NULL COMMENT '绑定时间',
+  PRIMARY KEY (`id`),
+  KEY `idx_unique_type_user` (`unique_id`,`type`,`user_id`) USING BTREE COMMENT '渠道用户索引'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- ----------------------------
+-- Records of member_user_social
+-- ----------------------------
+BEGIN;
+COMMIT;
+
+-- ----------------------------
+-- Table structure for member_user_tenant
+-- ----------------------------
+DROP TABLE IF EXISTS `member_user_tenant`;
+CREATE TABLE `member_user_tenant` (
+  `id` bigint NOT NULL COMMENT 'ID',
+  `user_id` bigint NOT NULL COMMENT '用户ID',
+  `tenant_id` bigint NOT NULL COMMENT '租户ID',
+  `main` bit(1) NOT NULL COMMENT '是否为主要租户',
+  `name` varchar(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '租户名称',
+  `avatar` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL COMMENT '头像',
+  `created_at` datetime NOT NULL COMMENT '创建时间',
+  PRIMARY KEY (`id`),
+  KEY `idx_user` (`user_id`) USING BTREE,
+  KEY `idx_tenant_user` (`tenant_id`,`user_id`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- ----------------------------
+-- Records of member_user_tenant
+-- ----------------------------
+BEGIN;
+COMMIT;
+
+SET FOREIGN_KEY_CHECKS = 1;
